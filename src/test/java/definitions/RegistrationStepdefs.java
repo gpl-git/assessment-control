9daa69f3c85a9a9ef2ea7b84d1e504cb33bc2c00
Galina <gplagun@yahoo.com>
2020-09-01 18:36:55 -0400

    Registration Happy Path

---
+++ src/test/java/definitions/RegistrationStepdefs.java
@@ -0,0 +1,67 @@
+package definitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static support.TestContext.getDriver;
+
+public class RegistrationStepdefs {
+    @Given("I open {string} page")
+    public void iOpenPage(String page) {
+        if (page.equals("registration")){
+            getDriver().get("http://ask-qa.portnov.com/#/registration");
+        }else if (page.equals("login")){
+            getDriver().get("http://ask-qa.portnov.com/#/login");
+
+        }else{
+            System.out.println("Undefined page");
+        }
+    }
+
+    @When("I type {string} into firstName field")
+    public void iTypeIntoFirstNameField(String firstName) {
+        getDriver().findElement(By.xpath("//*[@formcontrolname='firstName']")).sendKeys(firstName);
+
+    }
+
+    @When("I type {string} into lastName field")
+    public void iTypeIntoLastNameField(String lastName) {
+        getDriver().findElement(By.xpath("//*[@formcontrolname='lastName']")).sendKeys(lastName);
+    }
+
+    @When("I type {string} into email field")
+    public void iTypeIntoEmailField(String email) {
+        getDriver().findElement(By.xpath("//*[@formcontrolname='email']")).sendKeys(email);
+    }
+
+    @When("I type {string} into group field")
+    public void iTypeIntoGroupField(String groupCode) {
+        getDriver().findElement(By.xpath("//*[@formcontrolname='group']")).sendKeys(groupCode);
+    }
+
+    @When("I type {string} into password field")
+    public void iTypeIntoPasswordField(String password) {
+        getDriver().findElement(By.xpath("//*[@formcontrolname='password']")).sendKeys(password);
+
+    }
+
+    @When("I type {string} into confirmPassword field")
+    public void iTypeIntoConfirmPasswordField(String confPassword) {
+        getDriver().findElement(By.xpath("//*[@formcontrolname='confirmPassword']")).sendKeys(confPassword);
+    }
+
+    @When("I click {string} button")
+    public void iClickButton(String btnName) throws InterruptedException {
+        getDriver().findElement(By.xpath("//span[text()='"+btnName+"']")).click();
+        Thread.sleep(2000);
+    }
+
+    @Then("confirmation message {string} is displayed")
+    public void confirmationMessageIsDisplayed(String message) {
+        String actualMessage = getDriver().findElement(By.xpath("//mat-card[@class='mat-card']/h4")).getText();
+        assertThat(actualMessage.equals(message)).isTrue();
+    }
+}
--- src/test/resources/features/registration.feature
+++ src/test/resources/features/registration.feature
@@ -1,8 +1,19 @@
-git@smoke
+@smoke
 Feature: Registration Scenarios
-
   @smoke
-  Scenario: Registration FirstName
+  Scenario: Registration Happy Path
+    Given I open "registration" page
+    When I type "First" into firstName field
+    And I type "Last" into lastName field
+    And I type "test@test.com" into email field
+    When I type "ABC" into group field
+    And I type "12345" into password field
+    When I type "12345" into confirmPassword field
+    When I click "Register Me" button
+    Then confirmation message "You have been Registered." is displayed
+
+
+
 
 
   
