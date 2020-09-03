722714fb1d5304c4e1b5d46931bdf4b381e4fce4
Galina <gplagun@yahoo.com>
2020-09-03 16:14:42 -0400

    test

--- src/test/java/definitions/RegistrationStepdefs.java
+++ src/test/java/definitions/RegistrationStepdefs.java
@@ -4,6 +4,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
 import org.openqa.selenium.By;
 
 import static org.assertj.core.api.Assertions.assertThat;
