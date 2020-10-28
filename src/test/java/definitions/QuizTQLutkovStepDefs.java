0a920521ffac2b35854ce61a95b303be538fc921
elut89 <@Werder89>
2020-10-28 11:25:45 -0700

    created files

---
+++ src/test/java/definitions/QuizTQLutkovStepDefs.java
@@ -0,0 +1,9 @@
+package definitions;
+
+import cucumber.api.java.en.Given;
+
+public class QuizTQLutkovStepDefs {
+    @Given("I navigate to {string} page")
+    public void iNavigateToPage(String arg0) {
+    }
+}
---
+++ src/test/resources/features/quiz_tq_lutkov.feature
@@ -0,0 +1,6 @@
+@quiz
+Feature: Quiz-Total Questions Scenarios
+
+  @quiz1
+  Scenario: 1 Textual Question Submitted
+    Given I navigate to "url" page
