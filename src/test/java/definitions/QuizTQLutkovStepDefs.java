907ad5e2c37af0efbda8d0321e370ec21c0c2b60
elut89 <@Werder89>
2020-10-28 20:14:17 -0700

    add

--- src/test/java/definitions/QuizTQLutkovStepDefs.java
+++ src/test/java/definitions/QuizTQLutkovStepDefs.java
@@ -1,9 +1,60 @@
 package definitions;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static support.TestContext.getDriver;
 
 public class QuizTQLutkovStepDefs {
     @Given("I navigate to {string} page")
-    public void iNavigateToPage(String arg0) {
+    public void iNavigateToPage(String url) {
+        if(url.equals("Sign In")){
+            getDriver().get("http://ask-qa.portnov.com/#/login");
+        } else {
+            System.out.println("Unsupported site " + url);
+        }
+    }
+
+    @When("I {string} with email: {string} and password: {string}")
+    public void iWithEmailAndPassword(String action, String email, String pswd) {
+        WebElement emailElement = getDriver().findElement(By.xpath("//input[@formcontrolname='email']"));
+        WebElement pswdElement = getDriver().findElement(By.xpath("//input[@formcontrolname='password']"));
+        WebElement btnElement = getDriver().findElement(By.xpath("//div[@class='controls']/descendant::span[contains(text(),'" + action + "')]"));
+
+        new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@formcontrolname='email']")));
+        new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@formcontrolname='password']")));
+        new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.elementToBeClickable(btnElement));
+
+        if(action.equals("Sign In")){
+            emailElement.sendKeys(email);
+            pswdElement.sendKeys(pswd);
+            btnElement.click();
+        }
+        else {
+            System.out.println("In construction"); // todo later
+        }
+    }
+
+
+    @Then("I check user role as {string}")
+    public void iCheckUserRoleAs(String userRole) {
+        new WebDriverWait(getDriver(), 10, 200).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='info']")));
+
+        String userInfo = getDriver().findElement(By.xpath("//div[@class='info']")).getText();
+        //assertThat(userInfo.contains(userRole)).isTrue();
+
+        //String userInfo = getDriver().findElement(By.xpath("//p[contains(text(),'TEACHER')]"));
+        //System.out.println(userInfo);
+        boolean userRoleBoolean = userInfo.toUpperCase().contains(userRole.toUpperCase());
+        Assert.assertEquals(userRoleBoolean, true, "Only 'Teacher' user role is good for this test scenario.");
+
     }
 }
--- src/test/resources/features/quiz_tq_lutkov.feature
+++ src/test/resources/features/quiz_tq_lutkov.feature
@@ -3,4 +3,12 @@ Feature: Quiz-Total Questions Scenarios
 
   @quiz1
   Scenario: 1 Textual Question Submitted
-    Given I navigate to "url" page
+    Given I navigate to "Sign In" page
+    When I "Sign In" with email: "ask_instr@aol.com" and password: "12345"
+    #And I wait for 2 sec
+    Then I check user role as "Teacher"
+
+
+
+
+    And I wait for 5 sec
