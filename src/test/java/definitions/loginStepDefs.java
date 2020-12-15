99683de9a7678abf6d0069ae9cba2f873677294c
Bohdan <bogdan.lypko@gmail.com>
2020-12-14 21:35:35 -0600

    saving data in definitions

--- src/test/java/definitions/loginStepDefs.java
+++ src/test/java/definitions/loginStepDefs.java
@@ -6,38 +6,51 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
-public class loginStepDefs {
+public class LoginStepDefs {
     @Given("I open {string}")
     public void iOpen(String url) {
-        getDriver().get("http://ask-qa.portnov.com/#/login");
+        if (url.equals("login")) {
+            getDriver().get("http://ask-qa.portnov.com/#/login");
+        }else if (url.equals("registration")){
+            getDriver().get("http://ask-qa.portnov.com/#/registration");
+        }else{
+            System.out.println("Unsupported site");
+        }
     }
 
-    @And("I wair for {int} sec")
-    public void iWairForSec(int sec) throws InterruptedException {
+    @And("I wait for {int} sec")
+    public void iWaitForSec(int sec) throws InterruptedException {
         Thread.sleep(sec * 1000);
     }
 
-    @When("I type {string} as email")
-    public void iTypeAsEmail(String email) {
-        getDriver().findElement(By.xpath("//input[@ formcontrolname='email']")).sendKeys(email);
+    @When("I enter {string} as email")
+    public void iEnterAsEmail(String email) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='email']")).sendKeys(email);
+
     }
 
-    @And("I type {string} in pw")
-    public void iTypeInPw(String pw) {
-        getDriver().findElement(By.id("mat-input-1")).sendKeys(pw);
+    @And("I enter {string} as password")
+    public void iEnterAsPassword(String pwd) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='password']")).sendKeys(pwd);
+
     }
 
-    @When("I click sing in")
-    public void iClickSingIn() {
-        getDriver().findElement(By.xpath("//span[contains(text(),'Sign In')]")).click();
+    @When("I click {string} button")
+    public void iClickButton(String btnName) {
+        getDriver().findElement(By.xpath("//span[contains(text(),'"+btnName+"')]")).click();
     }
 
     @Then("I verify that user role is equal to {string}")
     public void iVerifyThatUserRoleIsEqualTo(String userRole) {
         String actualRole = getDriver().findElement(By.xpath("//header")).getText();
         assertThat(actualRole.contains(userRole)).isTrue();
+    }
+
+    @And("I click {string} link")
+    public void iClickLink(String link) {
+        getDriver().findElement(By.xpath("//h5[contains(text(),'"+link+"')]")).click();
     }
 }
