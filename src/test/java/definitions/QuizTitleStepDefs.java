e2ca467f123edd094b86a7125b7daea101ac883a
Irina Izvekova <irina.izvekova7@gmail.com>
2021-02-23 19:30:49 -0800

    Created feature file and stepdefinitions file for Quiz Title Scenarios

--- src/test/java/definitions/QuizTitleStepDefs.java
+++ src/test/java/definitions/QuizTitleStepDefs.java
@@ -6,43 +6,55 @@ import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.openqa.selenium.By.*;
 import static support.TestContext.getDriver;
 
 public class QuizTitleStepDefs {
     @When("I click on {string} button")
     public void iClickOnButton(String btn) {
-        getDriver().findElement(By.xpath("//h5[contains(text(),'Quizzes')]")).click();
+        getDriver().findElement(xpath("//h5[contains(text(),'Quizzes')]")).click();
     }
 
 
     @And("I type {string} in the Title field")
     public void iTypeInTheTitleField(String title) {
-        getDriver().findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys(title);
+        getDriver().findElement(xpath("//input[@formcontrolname='name']")).sendKeys(title);
 
     }
 
 
     @Then("I click {string}")
     public void iClick(String btn) {
-        getDriver().findElement(By.xpath("//body/ac-root[1]/mat-sidenav-container[1]/mat-sidenav-content[1]/main[1]/ac-quiz-builder-page[1]/form[1]/div[1]/button[1]/span[1]")).click();
+        getDriver().findElement(xpath("//mat-icon[text()='add_circle']")).click();
     }
 
 
     @Then("quiz page should be displayed")
     public void quizPageShouldBeDisplayed() {
-        getDriver().findElement(By.xpath("//h3[contains(text(),'Passing Rate')]")).isDisplayed();
+        getDriver().findElement(xpath("//h3[contains(text(),'Passing Rate')]")).isDisplayed();
     }
 
 
     @When("I clear Title field")
     public void iClearTitleField() {
-        getDriver().findElement(By.xpath("//input[@placeholder='Title Of The Quiz *']")).clear();
+        getDriver().findElement(xpath("//input[@placeholder='Title Of The Quiz *']")).clear();
     }
 
     @Then("error message {string} is displayed")
     public void errorMessageIsDisplayed(String message) {
-        String actual=getDriver().findElement(By.xpath("")).getText();
+        String actual=getDriver().findElement(xpath("//mat-error[contains(text(),'"+message+"')]")).getText();
         assertThat(actual.contains(message)).isTrue();
+    }
+
+    @When("I click on {string} field")
+    public void iClickOnField(String title) {
+        getDriver().findElement(xpath("//input[@placeholder='Title Of The Quiz *']")).click();
+    }
+
+
+    @And("you confirm delete action")
+    public void youConfirmDeleteAction() {
+        getDriver().findElement(xpath("//div[@class='mat-dialog-actions']//span[text()='Delete']")).click();
     }
 }
 
