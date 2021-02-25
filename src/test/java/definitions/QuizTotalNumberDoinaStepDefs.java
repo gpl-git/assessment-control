a617140265378d75ade413a01cc493cbcc6ca434
Diditzai93 <diditzai93@gmail.com>
2021-02-25 14:12:02 -0600

    Doina Quiz Total Number

--- src/test/java/definitions/QuizTotalNumberDoinaStepDefs.java
+++ src/test/java/definitions/QuizTotalNumberDoinaStepDefs.java
@@ -2,15 +2,21 @@ package definitions;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
+import static java.lang.Thread.sleep;
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class QuizTotalNumberDoinaStepDefs {
     @And("I verify that {string} button is present")
     public void iVerifyThatButtonIsPresent(String expbutton) {
-        String quizzes=getDriver().findElement(By.xpath("//*[text()='Quizzes']")).getText();
+        String quizzes=getDriver().findElement(By.xpath("//*[text()='"+expbutton+"']")).getText();
         assertThat(quizzes.contains(expbutton)).isTrue();
 
 
@@ -18,18 +24,122 @@ public class QuizTotalNumberDoinaStepDefs {
 
 
     @Then("I click on {string} button")
-    public void iClickOnButton(String quizzes) {
-        getDriver().findElement(By.xpath("//*[text()='Quizzes']")).click();
+    public void iClickOnButton(String forquiz) {
+        getDriver().findElement(By.xpath("//h5[contains(text(),'"+forquiz+"')]")).click();
     }
 
-    @And("I verify that Quizzes {string} is displayed")
-    public void iVerifyThatQuizzesIsDisplayed(String expmatcard) {
-        assertThat(getDriver().findElement(By.xpath("//div[@class='quizzes']")).isDisplayed()).isTrue();
 
+
+
+
+
+
+//    @And("I click on {string} tab")
+//    public void iClickOnTab(String newquiz) {
+//      getDriver().findElement(By.xpath("//*[@class='mat-button-wrapper']")).click();
+//    }
+
+    @Then("I type {string} into quiz title")
+    public void iTypeIntoQuizTitle(String title) {
+       getDriver().findElement(By.xpath("//*[@formcontrolname='name']")).sendKeys(title);
+    }
+
+
+    @And("I click on the plus button")
+    public void iClickOnThePlusButton() throws InterruptedException {
+        for (int i=0 ;i< 50;i++) {
+            getDriver().findElement(By.xpath("//mat-icon[contains(text(),'add_circle')]")).click();
+            Thread.sleep(2000);
+        }
+    }
+
+
+    @Then("I verify that {string} is displayed")
+    public void iVerifyThatIsDisplayed(String lastquest) {
+        List<WebElement> lastquest50 =getDriver().findElements(By.xpath("//mat-panel-title[contains(text(),'Q50')]"));
+
+        for (WebElement element:lastquest50) {
+            if (element.getText().contains(lastquest)) {
+                element.isDisplayed();
+            }
+        }
+             {
+
+        }
 
     }
 
-    @And("I click on {string} tab")
-    public void iClickOnTab(String create) {
+
+
+    @And("I click on the plus button {int}")
+    public void iClickOnThePlusButton(int max1) throws InterruptedException {
+        for (int i=0 ;i< max1;i++) {
+            getDriver().findElement(By.xpath("//mat-icon[contains(text(),'add_circle')]")).click();
+            Thread.sleep(2000);
+
+    }
+}
+
+
+    @Then("I select {string}")
+    public void iSelect(String select) {
+        WebElement element = getDriver().findElement(By.xpath("//label[contains(text(),'"+select+"')]"));
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("arguments[0].click();", element);
+
+    }
+
+    @And("I select group {string}")
+    public void iSelectGroup(String group) {
+        WebElement element = getDriver().findElement(By.xpath("//mat-list-option[24]"));
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("arguments[0].click();", element);
+    }
+
+    @Then("I slect quiz  {string}")
+    public void iSlectQuiz(String quiz) {
+        WebElement element = getDriver().findElement(By.xpath("//div[@dir='ltr']/../../../..//span[contains(text(),'Graded automatically')]"));
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("arguments[0].click();", element);
+
+    }
+
+//    @Then("I click on student name")
+//    public void iClickOnStudentName() {
+//        WebElement element = getDriver().findElement(By.xpath("//mat-pseudo-checkbox[@class='mat-pseudo-checkbox']/..//*[text()='N001']"));
+//        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+//        executor.executeScript("arguments[0].click();", element);
+//
+//    }
+
+    @And("I click {string}")
+    public void iClick(String assigment) {
+      WebElement element = getDriver().findElement(By.xpath("//span[contains(text(),'Give Assignment')]"));
+      JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+      executor.executeScript("arguments[0].click();", element);
+       // getDriver().findElement(By.xpath("//span[contains(text(),'Give Assignment')]")).click();
+
+    }
+
+    @Then("I click to {string}")
+    public void iClickTo(String logout) {
+        WebElement element = getDriver().findElement(By.xpath("//mat-dialog-container[@role='dialog']//span[contains(text(),'Log Out')]"));
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("arguments[0].click();", element);
+
+    }
+
+    @Then("I choose a {string}")
+    public void iChooseA(String student) {
+        getDriver().findElement(By.xpath("//mat-list-option[29]")).click();
+    }
+
+    @When("I choose {string}")
+    public void iChoose(String myassignments) {
+        WebElement element = getDriver().findElement(By.xpath("//tr[@class='ng-star-inserted']//td[contains(text(),'Graded automatically')]/..//span[contains(text(),'Go To Assessment')]"));
+        JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+        executor.executeScript("arguments[0].click();", element);
+
+       ;
     }
 }
--- src/test/resources/features/quizTotalNumberDoina.feature
+++ src/test/resources/features/quizTotalNumberDoina.feature
@@ -1,9 +1,9 @@
 @quizTotalNumberDoina @somke
 
-  Feature: Quiz Total Number Scenarios
+  Feature: Quiz Total Number  Of Questions Scenarios
 
-    @smoke
-       Scenario: Quiz Total Number Happy Path
+    @smoke1
+       Scenario: Quiz Total Number Of Questions Happy Path
          Given I go to "ask"
          When I enter "doina.qa922@gmail.com" into email field
          And I enter "Password123" into password field
@@ -12,8 +12,73 @@
          Then I verify that user is logged in at "home" page
          And I verify that "Quizzes" button is present
          Then I click on "Quizzes" button
-         Then  I wait for 1 sec
-         And I verify that Quizzes "mat-card" is displayed
-         And I click on "Create New quiz" tab
+         Then I wait for 1 sec
+         And I click "Create New Quiz" button
+         And I wait for 1 sec
+         Then I type "Total  Number of questions " into quiz title
+         And  I click on the plus button 50
+         Then I wait for 5 sec
+         Then I verify that "Q50" is displayed
+
+
+       @smoke2
+       Scenario: Quiz Total Number +1 Of Questions
+         Given I go to "ask"
+         When I enter "doina.qa922@gmail.com" into email field
+         And I enter "Password123" into password field
+         When  I click "Sign In" button
+         And I wait for 3 sec
+         Then I verify that user is logged in at "home" page
+         And I verify that "Quizzes" button is present
+         Then I click on "Quizzes" button
+         Then I wait for 3 sec
+         And I click "Create New Quiz" button
+         And I wait for 1 sec
+         Then I type "Total  Number of questions " into quiz title
+         And  I click on the plus button 51
+         Then I wait for 5 sec
+         Then I should see error message "Should contain no more then 50 Questions"
+         #Nowing bug I should see error message "Should contain no more then 50 Questions"
+         # Jira Bug https://jira.portnov.com/browse/AS-525
+
+        @smoke3
+        Scenario: Graded Automatically
+          Given I go to "ask"
+          When I enter "doina.qa922@gmail.com" into email field
+          And I enter "Password123" into password field
+          When  I click "Sign In" button
+          And I wait for 3 sec
+          Then I verify that user is logged in at "home" page
+          Then I verify that "Assignments" button is present
+          And I click on "Assignments" button
+          Then I wait for 3 sec
+          And I click "Create New Assignment" button
+          #Then I select "Group Filter"
+          Then I wait for 3 sec
+          Then I choose a "Student"
+          #And I select group "N001"
+          And I wait for 2 sec
+          Then I select "Select Quiz To Assign"
+          Then I slect quiz  "Graded automatically"
+          And I wait for 2 sec
+          And  I click "Give Assignment"
+          Then I click on "Log Out" button
+          And I wait for 2 sec
+          Then I click to "Log Out"
+          And I wait for 3 sec
+          When I enter "student1@test.com" into email field
+          And I enter "12345" into password field
+          When I click "Sign In" button
+          And I wait for 1 sec
+          Then I verify that user is logged in at "home" page
+          And   I click on "My Assignments" button
+          When I choose "My Assignments"
+
+
+
+
+
+
+
 
 
