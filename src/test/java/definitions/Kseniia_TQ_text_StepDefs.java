4d2e1a875b6df2b8fa2695611b33e76cdf5d36fe
Kseniia Serpukhova <kseniiaserpukhova@Kseniias-MacBook-Pro.local>
2021-05-28 16:22:53 -0500

    origin

--- src/test/java/definitions/Kseniia_TQ_text_StepDefs.java
+++ src/test/java/definitions/Kseniia_TQ_text_StepDefs.java
@@ -1,15 +1,17 @@
 package definitions;
 
+import com.sun.codemodel.JForLoop;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
-public class QuizStepDefs {
+public class Kseniia_TQ_text_StepDefs {
     @Given("I open {string} page")
     public void iOpenPage(String url) {
         if (url.equalsIgnoreCase("login")){
@@ -90,6 +92,34 @@ public class QuizStepDefs {
         getDriver().findElement(By.xpath("//div[@class='mat-dialog-actions']//*[text()='Delete']")).click();
         Thread.sleep(2000);
 
+
+    }
+
+
+    @Then("{string} error message should be displayed")
+    public void errorMessageShouldBeDisplayed(String errorMsg) {
+        assertThat(getDriver().findElement(By.xpath("//snack-bar-container[@role='alert']")).isDisplayed()).isTrue();
+    }
+
+    @When("I type {int} as question {string} text field {string}")
+    public void iTypeAsQuestionTextField(int numberChar, String questionNumber, String questionName) {
+        getDriver().findElement(By.xpath("//*[contains(text(),'"+questionNumber+"')]/../../..//*[@placeholder='Question *']")).sendKeys(Keys.chord("What is Scrum?\n" +
+                "Scrum is a framework that helps teams work together. Much like a rugby team (where it gets its name) training for the big game, scrum encourages teams to learn through experiences, self-organize while working on a problem, and reflect on their wins and losses to continuously improve.\n" +
+                "\n" +
+                "While the scrum I’m talking about is most frequently used by software development teams, its principles and lessons can be applied to all kinds of teamwork. This is one of the reasons scrum is so popular. Often thought of as an agile project management framework, scrum describes a set of meetings, tools, and roles that work in concert to help teams structure and manage their work.\n" +
+                "\n" +
+                "In this article, we’ll discuss how a traditional scrum framework is comprised with the help of the Scrum Guide and David West, CEO of Scrum.org. We’ll also include examples of how we see our customers stray from these fundamentals to fit their specific needs. For that, our own Megan Cook, Group Product Manager for Jir"));
+
+    }
+
+    @When("I type max+{int} as question {string} text field {string}")
+    public void iTypeMaxAsQuestionTextField(int numberChar, String questionNumber, String questionName) {
+        getDriver().findElement(By.xpath("//*[contains(text(),'"+questionNumber+"')]/../../..//*[@placeholder='Question *']")).sendKeys(Keys.chord("What is Scrum?\n" +
+                "Scrum is a framework that helps teams work together. Much like a rugby team (where it gets its name) training for the big game, scrum encourages teams to learn through experiences, self-organize while working on a problem, and reflect on their wins and losses to continuously improve.\n" +
+                "\n" +
+                "While the scrum I’m talking about is most frequently used by software development teams, its principles and lessons can be applied to all kinds of teamwork. This is one of the reasons scrum is so popular. Often thought of as an agile project management framework, scrum describes a set of meetings, tools, and roles that work in concert to help teams structure and manage their work.\n" +
+                "\n" +
+                "In this article, we’ll discuss how a traditional scrum framework is comprised with the help of the Scrum Guide and David West, CEO of Scrum.org. We’ll also include examples of how we see our customers stray from these fundamentals to fit their specific needs. For that, our own Megan Cook, Group Product Manager for Jira"));
 
     }
 }
