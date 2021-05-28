8a8d2bb5466e92a135bbaf4ff16bd07b687cc539
ViktarSh <shagunviktor@gmail.com>
2021-05-28 03:38:33 -0400

    Viktars Update

--- src/test/java/defenitions/QuizStepDef.java
+++ src/test/java/defenitions/QuizStepDef.java
@@ -4,6 +4,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -12,14 +13,12 @@ import static support.TestContext.getDriver;
 public class QuizStepDef {
     @Given("I open {string} page")
     public void iOpenPage(String url) {
-        if (url.equalsIgnoreCase("login")){
-            getDriver().get ("http://ask-qa.portnov.com/#/login");
-        }
-        else if (url.equalsIgnoreCase("registration")){
+        if (url.equalsIgnoreCase("login")) {
+            getDriver().get("http://ask-qa.portnov.com/#/login");
+        } else if (url.equalsIgnoreCase("registration")) {
             getDriver().get("http://ask-qa.portnov.com/#/registration");
-        }
-        else{
-            System.out.println("Site" +url+ " is not supported");
+        } else {
+            System.out.println("Site" + url + " is not supported");
         }
     }
 
@@ -40,12 +39,12 @@ public class QuizStepDef {
 
     @When("I click {string} button")
     public void iClickButton(String btnName) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'"+btnName+"')]")).click();
+        getDriver().findElement(By.xpath("//span[contains(text(),'" + btnName + "')]")).click();
     }
 
     @And("I click on {string} link")
     public void iClickOnLink(String link) {
-        getDriver().findElement(By.xpath("//h5[text()='"+link+"']")).click();
+        getDriver().findElement(By.xpath("//h5[text()='" + link + "']")).click();
     }
 
     @Given("I type {string} as quiz title")
@@ -60,37 +59,93 @@ public class QuizStepDef {
 
     @And("I select {string} question type")
     public void iSelectQuestionType(String type) {
-        getDriver().findElement(By.xpath("//*[contains(text(),'"+type+"')]")).click();
+        getDriver().findElement(By.xpath("//*[contains(text(),'" + type + "')]")).click();
     }
 
     @When("I type {string} as question {string} text field {string}")
     public void iTypeAsQuestionTextField(String questionText, String questionNumber, String questionField) {
-        getDriver().findElement(By.xpath("//*[contains(text(),'"+questionNumber+"')]/../../..//*[@placeholder='Question *']")).sendKeys(questionText);
+        getDriver().findElement(By.xpath("//*[contains(text(),'" + questionNumber + "')]/../../..//*[@placeholder='Question *']")).sendKeys(questionText);
     }
 
     @And("I type {string} as question {string} option field {string}")
     public void iTypeAsQuestionOptionField(String text, String questionNumber, String optionNumber) {
-        getDriver().findElement(By.xpath("//*[contains(text(), '"+questionNumber+"')]/../../..//*[@placeholder='"+optionNumber+"']")).sendKeys(text);
+        int length = 1000;
+        boolean useLetters = true;
+        boolean useNumbers = true;
+        String generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+        text = generatedString;
+        getDriver().findElement(By.xpath("//*[contains(text(), '" + questionNumber + "')]/../../..//*[@placeholder='" + optionNumber + "']")).sendKeys(text);
     }
 
     @When("I check {string} as correct answer in {string}")
     public void iCheckAsCorrectAnswerIn(String optionNumber, String questionNumber) {
-        getDriver().findElement(By.xpath("//*[contains(text(),'"+questionNumber+"')]/../../..//*[@placeholder='"+optionNumber+"']/../../../../../mat-radio-button")).click();
+        getDriver().findElement(By.xpath("//*[contains(text(),'" + questionNumber + "')]/../../..//*[@placeholder='" + optionNumber + "']/../../../../../mat-radio-button")).click();
     }
 
     @Then("quiz {string} should be displayed on the list of quizzess")
     public void quizShouldBeDisplayedOnTheListOfQuizzess(String quizTitle) {
-        assertThat(getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+quizTitle+"')]")).isDisplayed()).isTrue();
+        assertThat(getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + quizTitle + "')]")).isDisplayed()).isTrue();
     }
 
     @And("I delete {string} from the list of quizzes")
     public void iDeleteFromTheListOfQuizzes(String quizTitle) throws InterruptedException {
-        getDriver().findElement(By.xpath("//*[contains(text(),'"+quizTitle+"')]")).click();
+        getDriver().findElement(By.xpath("//*[contains(text(),'" + quizTitle + "')]")).click();
         Thread.sleep(1000);
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+quizTitle+"')]/../../..//span[text()='Delete']")).click();
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + quizTitle + "')]/../../..//span[text()='Delete']")).click();
         getDriver().findElement(By.xpath("//div[@class='mat-dialog-actions']//*[text()='Delete']")).click();
         Thread.sleep(2000);
     }
 
 
+    @When("I add options in {string} up to {int}")
+    public void iAddOptionsInUpTo(String questionNumber, int numberOption) throws InterruptedException {
+        String buttonAdd = "//*[contains(text(),'" + questionNumber + "')]/../../..//*[contains(text(),'Add Option')]";
+        for (int i = 3; i <= numberOption; i++) {
+            getDriver().findElement(By.xpath(buttonAdd)).click();
+            String xpath = "//*[contains(text(),'" + questionNumber + "')]/../../..//*[@placeholder = 'Option " + i + "*']";
+            Thread.sleep(1000);
+            getDriver().findElement(By.xpath(xpath)).sendKeys("Option " + i);
+        }
+    }
+
+    @Then("quiz {string} shouldn`t be displayed on the list of quizzess")
+    public void quizShouldnTBeDisplayedOnTheListOfQuizzess(String quizTitle) {
+        assertThat(getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + quizTitle + "')]")).isDisplayed()).isFalse();
+    }
+
+    @Then("{string} should appear")
+    public void shouldAppear(String error) {
+        assertThat(getDriver().findElement(By.xpath("//*[contains(text(), 'Check highlighted')]")).isDisplayed()).isTrue();
+    }
+
+    @And("I type {string} as question {string} option {string}")
+    public void iTypeAsQuestionOption(String text, String questionNumber, String optionNumber) {
+        int length = 1;
+        boolean useLetters = true;
+        boolean useNumbers = true;
+        String generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+        text = generatedString;
+        getDriver().findElement(By.xpath("//*[contains(text(), '" + questionNumber + "')]/../../..//*[@placeholder='" + optionNumber + "']")).sendKeys(text);
+    }
+
+    @When("I add options in {string} more than {int}")
+    public void iAddOptionsInMoreThan(String questionNumber, int numberOption) throws InterruptedException {
+        String buttonAdd = "//*[contains(text(),'" + questionNumber + "')]/../../..//*[contains(text(),'Add Option')]";
+        for (int i = 3; i <= numberOption; i++) {
+            getDriver().findElement(By.xpath(buttonAdd)).click();
+            String xpath = "//*[contains(text(),'" + questionNumber + "')]/../../..//*[@placeholder = 'Option " + i + "*']";
+            Thread.sleep(1000);
+            getDriver().findElement(By.xpath(xpath)).sendKeys("Option " + i);
+        }
+    }
+
+    @And("I type {string} as question {string} opt field {string}")
+    public void iTypeAsQuestionOptField(String text, String questionNumber, String optionNumber) {
+        int length = 1001;
+        boolean useLetters = true;
+        boolean useNumbers = true;
+        String generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+        text = generatedString;
+        getDriver().findElement(By.xpath("//*[contains(text(), '" + questionNumber + "')]/../../..//*[@placeholder='" + optionNumber + "']")).sendKeys(text);
+    }
 }
--- src/test/resources/features/Viktar_single_choi_quest.feature
+++ src/test/resources/features/Viktar_single_choi_quest.feature
@@ -30,3 +30,117 @@
       Then quiz "Test SC Quiz" should be displayed on the list of quizzess
       And I delete "Test SC Quiz" from the list of quizzes
       And I wait for 2 sec
+
+    @testcase2
+    Scenario: Single Choice Question - Option: Max 1000 characters
+      Given I type "Test SC option" as quiz title
+      When I add a question
+      And I select "Single" question type
+      And I wait for 2 sec
+      When I type "Question1" as question "Q1" text field "Question *"
+      And I type "var1" as question "Q1" option field "Option 1*"
+      And I type "var2" as question "Q1" option field "Option 2*"
+      And I wait for 2 sec
+      When I click "Save" button
+      And I wait for 2 sec
+      Then quiz "Test SC option" should be displayed on the list of quizzess
+      When I delete "Test SC option" from the list of quizzes
+      And I wait for 1 sec
+
+    @testcase3
+    Scenario: Single Choice Question - Option: Max 15 choice
+      Given I type "15 Test SC option" as quiz title
+      When I add a question
+      And I select "Single" question type
+      And I wait for 2 sec
+      When I type "Question1" as question "Q1" text field "Question *"
+      And I type "var1" as question "Q1" option field "Option 1*"
+      And I type "var1" as question "Q1" option field "Option 2*"
+      And I wait for 2 sec
+      And I check "Option 1*" as correct answer in "Q1"
+      When I add options in "Q1" up to 15
+      When I click "Save" button
+      And I wait for 4 sec
+      Then quiz "15 Test SC option" should be displayed on the list of quizzess
+      And I wait for 1 sec
+
+      @testcase4
+      Scenario: Single Choice Question - Option: Max 1000 characters +1
+        Given I type "Test SC option" as quiz title
+        When I add a question
+        And I select "Single" question type
+        And I wait for 2 sec
+        When I type "Question1" as question "Q1" text field "Question *"
+        And I type "var1" as question "Q1" opt field "Option 1*"
+        And I type "var1" as question "Q1" opt field "Option 1*"
+        And I wait for 2 sec
+        And I check "Option 1*" as correct answer in "Q1"
+        When I click "Save" button
+        And I wait for 2 sec
+        Then quiz "Test SC option" shouldn`t be displayed on the list of quizzess
+        And I wait for 1 sec
+
+      @testcase5
+      Scenario: Single Choice Question - Option: Max 15 choice +1
+        Given I type "16 Test SC option" as quiz title
+        When I add a question
+        And I select "Single" question type
+        And I wait for 2 sec
+        When I type "Question1" as question "Q1" text field "Question *"
+        And I type "var1" as question "Q1" option field "Option 1*"
+        And I type "var1" as question "Q1" option field "Option 2*"
+        And I wait for 2 sec
+        And I check "Option 1*" as correct answer in "Q1"
+        When I add options in "Q1" more than 16
+        When I click "Save" button
+        And I wait for 4 sec
+        Then quiz "16 Test SC option" shouldn`t be displayed on the list of quizzess
+        And I wait for 1 sec
+
+      @testcase6
+      Scenario: Single Choice Question - Option: Cannot be empty
+        Given I type "Test SC option" as quiz title
+        When I add a question
+        And I select "Single" question type
+        And I wait for 2 sec
+        When I type "Question1" as question "Q1" text field "Question *"
+        And I type "var1" as question "Q1" option field "Option 1*"
+        And I wait for 2 sec
+        And I check "Option 1*" as correct answer in "Q1"
+        And I click "Save" button
+        And I wait for 2 sec
+        Then "error message" should appear
+
+      @testcase7
+      Scenario: Single Choice Question - Option: Min 1 character
+        Given I type "Test SC option" as quiz title
+        When I add a question
+        And I select "Single" question type
+        And I wait for 2 sec
+        When I type "Question1" as question "Q1" text field "Question *"
+        And I type "var1" as question "Q1" option "Option 1*"
+        And I type "var1" as question "Q1" option "Option 2*"
+        And I wait for 2 sec
+        And I check "Option 1*" as correct answer in "Q1"
+        And I click "Save" button
+        And I wait for 2 sec
+        Then quiz "Test SC option" should be displayed on the list of quizzess
+        When I delete "Test SC option" from the list of quizzes
+        And I wait for 1 sec
+
+      @testcase8
+      Scenario: Single Choice Question - Option: Min 2 choices
+        Given I type "Test SC option" as quiz title
+        When I add a question
+        And I select "Single" question type
+        And I wait for 2 sec
+        When I type "Question1" as question "Q1" text field "Question *"
+        And I type "var1" as question "Q1" option field "Option 1*"
+        And I type "var1" as question "Q1" option field "Option 2*"
+        And I wait for 3 sec
+        And I check "Option 1*" as correct answer in "Q1"
+        And I click "Save" button
+        And I wait for 2 sec
+        Then quiz "Test SC option" should be displayed on the list of quizzess
+        When I delete "Test SC option" from the list of quizzes
+        And I wait for 1 sec
