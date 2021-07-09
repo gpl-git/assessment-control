2bcd36642d2453d59208558638ffa75a8eac1c3b
Tatiana Kraskova <kraskova.qa@gmail.com>
2021-07-08 23:25:24 -0400

    quiz with 5 mult choice_first commit

---
+++ src/test/java/definitions/crQuiz5MultQuestStepDefTatK.java
@@ -0,0 +1,63 @@
+package definitions;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+
+import static support.TestContext.getDriver;
+
+public class crQuiz5MultQuestStepDefTatK {
+    String titleGenerator = RandomStringUtils.random(2, 1, 99, false, true);
+    String quizTitle = "QuizTitle" + titleGenerator;
+
+    @When("I select {string} as a correct option in {string}")
+    public void iSelectAsACorrectOptionIn(String optionNum, String questionNum) {
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + questionNum + "')]/../../..//textarea[@placeholder='" + optionNum + "']/../../../../../mat-checkbox")).click();
+    }
+
+    @And("I create {string} title")
+    public void iCreateTitle() {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys(quizTitle);
+    }
+
+    @When("I create quiz with {int} Multiple-Choice questions")
+    public void iCreateQuizWithMultipleChoiceQuestions(int numQuestions) throws InterruptedException {
+
+        if (numQuestions >= 1) {
+            getDriver().findElement(By.xpath("//*[@formcontrolname='name']")).sendKeys(quizTitle);
+            getDriver().findElement(By.xpath("//*[@class='mat-icon material-icons']")).click();
+            Thread.sleep(2000);
+
+            for (int i = 1; i < numQuestions; ++i) {
+                Thread.sleep(1000);
+                getDriver().findElement(By.xpath("//*[contains(text(),'Q" + i + "')]/../../..//*[contains(text(), 'Multiple')]")).click();
+                Thread.sleep(2000);
+                getDriver().findElement(By.xpath("//*[contains(text(),'Q" + i + "')]/../../..//*[@placeholder='Question *']")).sendKeys("Question example " + i + "");
+                Thread.sleep(2000);
+                getDriver().findElement(By.xpath("//textarea[@placeholder='Option 1*']")).sendKeys("Question " + i + "");
+                getDriver().findElement(By.xpath("//textarea[@placeholder='Option 2*']")).sendKeys("Question " + i + "");
+                Thread.sleep(2000);
+
+                //failed getDriver().findElement(By.xpath("//*[contains(text(),'Option "+i+"*')]/../../../../../..//input[@type='checkbox']")).click();
+                //failed getDriver().findElement(By.xpath("//div[@class='mat-checkbox-ripple mat-ripple']/../../../../..//*[contains(text(),'Option 1*')] ")).click();
+                //failed tDriver().findElement(By.xpath("//*[contains(text(),'Q "+i+"')]/../../../..//*[contains(text(),'Option "+i+"*')]/../../../../../..//input[@type='checkbox']")).click();
+                //failed getDriver().findElement(By.xpath("//*[contains(text(),'Option 1*')]/../../../../../..//*[@class='mat-checkbox-frame']")).click();
+                //failed  getDriver().findElement(By.xpath("//*[contains(text(),'Option 1*')]/../../../../../..//*[@class='mat-checkbox-label']")).click();
+                getDriver().findElement(By.xpath("//*[contains(text(),'Option 1*')]/../../../../../..//*[@type='checkbox']/..")).click();
+                Thread.sleep(2000);
+                getDriver().findElement(By.xpath("//mat-icon[contains(text(), 'add_circle')]")).click();
+                Thread.sleep(2000);
+            }
+
+//            getDriver().findElement(By.xpath("//*[contains(text(),'Q" + numQuestions + "')]/../../..//*[contains(text(),'Multiple-Choice')]")).click();
+//            getDriver().findElement(By.xpath("//*[contains(text(),'Q" + numQuestions + "')]/../../..//*[@placeholder='Question *']")).sendKeys("Question " + numQuestions + "");
+//            getDriver().findElement(By.xpath("//span[contains(text(),'Save')]")).click();
+        }
+    }
+}
+
+//failed getDriver().findElement(By.xpath("//*[@placeholder='Option "+i+"*']/../../../../..//input[@type='checkbox']/../../../..//*[contains(text(),'Option "+i+"*')]")).click();
+// failed getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + i + "')]/../../..//textarea[@placeholder='" + i + "']/../../../../../mat-checkbox")).click();
+// failed getDriver().findElement(By.xpath("//*[@placeholder='Option "+i+"*']/../../../../..//textarea[@placeholder='" + i + "']/../../../../../mat-checkbox")).click();
+// failed getDriver().findElement(By.xpath("//*[@placeholder='Option "+i+"*']/../../../../..//mat-checkbox")).click();
---
+++ src/test/resources/features/crQuiz5MultQuestTatK.feature
@@ -0,0 +1,41 @@
+@quiz @regression
+Feature: Quiz Scenarios with 5 Multiple-Choice questions
+
+  Background:
+    Given I go to "login" page
+    When I type "zhossam@ardesy.com" into email field
+    And I type "123456" into password field
+    When I click on "Sign In" button
+    And I wait for 1 sec
+    Then I verify current url as "home"
+    When I click on "Quizzes" tab
+    And I wait for 1 sec
+
+  @regression
+  Scenario: Create a quiz with 5 Multiple-Choice questions
+    When I click on "Create New Quiz" button
+    And I wait for 1 sec
+    And I type "TatianaK Quiz" as quiz title
+    When I add a question
+    And I select "Multiple-Choice" question type
+    And I wait for 3 sec
+    When I type question text "Question1" into "Q1"
+    And I type "Option 1" as option "Option 1*" into "Q1"
+    And I type "Option 2" as option "Option 2*" into "Q1"
+    And I wait for 3 sec
+    When I select "Option 2*" as a correct option in "Q1"
+    And I wait for 3 sec
+    When I click on "Save" button
+    And I wait for 3 sec
+    Then quiz "TatianaK Quiz" should be displayed on the list of quizzes
+    And I wait for 1 sec
+    And I delete "TatianaK Quiz" from the list of quizzes
+
+  @quiz @regression
+  Scenario: Quiz with 5 Multiple-Choice questions using loop
+    When I click on "Create New Quiz" button
+    And I wait for 3 sec
+    And I create quiz title
+    And I wait for 2 sec
+    When I create quiz with 5 Multiple-Choice questions
+    And I wait for 2 sec
