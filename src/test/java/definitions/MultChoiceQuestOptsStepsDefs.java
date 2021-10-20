31e6334ee9c1cdf3234f82a45940c8285ce90adc
volodin-victor <Pb3CFg@dm1N!>
2021-10-20 13:21:00 -0700

    Added Multiple Choice Options Field Doesn't Accepts 1000+1 char (max char + 1) scenario

--- src/test/java/definitions/MultChoiceQuestOptsStepsDefs.java
+++ src/test/java/definitions/MultChoiceQuestOptsStepsDefs.java
@@ -1,9 +1,12 @@
 package definitions;
 
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,5 +37,26 @@ public class MultChoiceQuestOptsStepsDefs {
     @When("I push button {string}")
     public void iPushButton(String btnName) {
         getDriver().findElement(By.xpath("//span[contains(text(),'" + btnName + "')]")).click();
+    }
+
+    @Then("{string} is not displayed on the list of quizzes")
+    public void isNotDisplayedOnTheListOfQuizzes(String quizTitle) {
+        assertThat(getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + quizTitle + "')]")).isDisplayed()).isFalse();
+    }
+
+    @And("I check and delete existed quizzes with name {string}")
+    public void iCheckAndDeleteExistedQuizzesWithName(String quizTitle) throws InterruptedException {
+        List<WebElement> quiz = getDriver().findElements(By.xpath("//mat-panel-title[contains(text(),'\" + quizTitle + \"')]"));
+        if (!quiz.isEmpty()) {
+            List<WebElement> webElements = getDriver().findElements(By.xpath("//mat-panel-title[contains(text(),'" + quizTitle + "')]/../../..//span[contains(text(),'Delete')]"));
+            if (!webElements.isEmpty()) {
+                for (WebElement webElement : webElements) {
+                    webElement.click();
+                    Thread.sleep(1500);
+                    getDriver().findElement(By.xpath("//div[@class='mat-dialog-actions']//span[text()='Delete']")).click();
+                    Thread.sleep(1500);
+                }
+            }
+        }
     }
 }
--- src/test/resources/features/MultChoiceQuesOpts.feature
+++ src/test/resources/features/MultChoiceQuesOpts.feature
@@ -9,7 +9,9 @@ Feature: Multiple Choice Question - Options
     And I wait for 1 sec
     When I click on "Quizzes" menu
     And I wait for 1 sec
+    And I check and delete existed quizzes with name "Dmitry K Multiple Choice Options Quiz"
 
+  @predefined1
   Scenario: Multiple Choice Option Field Accepts 1000 characters (maximum)
     When I click "Create New Quiz" button
     And I wait for 2 sec
@@ -23,12 +25,27 @@ Feature: Multiple Choice Question - Options
     And Input test-data 1000 chars into field "Option 1"
     And Input test-data 1000 chars into field "Option 2"
     When I push button "Save"
-    And I wait for 2 sec
+    And I wait for 1 sec
     Then "Dmitry K Multiple Choice Options Quiz" is displayed on the list of quizzes
-    And I wait for 3 sec
+    And I wait for 1 sec
     And I delete "Dmitry K Multiple Choice Options Quiz" from the list of quizzes
 
 
-
+  @predefined2
+  Scenario: Multiple Choice Options Field Doesn't Accepts 1000+1 char (max char + 1)
+    When I click "Create New Quiz" button
+    And I wait for 2 sec
+    When I type "Dmitry K Multiple Choice Options Quiz" as quiz title
+    And I add a question
+    When I select "Multiple-Choice" question type
+    And I wait for 2 sec
+    And I type "Multiple Choice Question 1" into "Q1"
+    When I checked "Option 1"
+    And I checked "Option 2"
+    And Input test-data 1001 chars into field "Option 1"
+    And Input test-data 1001 chars into field "Option 2"
+    When I push button "Save"
+    And I wait for 1 sec
+    Then "Dmitry K Multiple Choice Options Quiz" is not displayed on the list of quizzes
 
 
