6563b6792188c320ba35a0d52c8f290d5a0f6656
dixistenz <dvikulin@gmail.com>
2021-10-19 22:44:17 -0400

    updating master

--- src/test/java/definitions/PredefinedStepDefs.java
+++ src/test/java/definitions/PredefinedStepDefs.java
@@ -4,6 +4,9 @@ import cucumber.api.java.en.Given;
 
 public class PredefinedStepDefs {
     @Given("I open {string} page")
-    public void iOpenPage(String arg0) {
+    public void iOpenPage(String url) {
+        if (url.equals("login")){
+            getDriver()
+        }
     }
 }
