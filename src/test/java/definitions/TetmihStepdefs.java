8b1c3e6be1dc30529895fc621934a13e7ee783d6
Tetyana Mykhalska <tatiana@tatianas-imac.myfiosgateway.com>
2022-01-26 20:34:44 -0500

    Tetmih feature file created

--- src/test/java/definitions/TetmihStepdefs.java
+++ src/test/java/definitions/TetmihStepdefs.java
@@ -1,2 +1,8 @@
 package definitions;
 
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static support.TestContext.getDriver;
