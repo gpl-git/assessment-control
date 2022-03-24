31ed2b3eaa338c2c02eaf92745b43679e8dd7937
unknown <igorkubyshev@gmail.com>
2022-03-23 22:17:10 -0400

    V1

--- src/test/java/definitions/TextStepdefs.java
+++ src/test/java/definitions/TextStepdefs.java
@@ -9,21 +9,15 @@ import org.openqa.selenium.By;
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
-public class QuizStepDefs {
+public class TextStepdefs {
     @Given("I go to {string} page")
     public void iGoToPage(String url) {
-        if (url.equals("login")) {
-            getDriver().get("http://ask-qa.portnov.com/#/login");
-        }else if(url.equals("registration")){
-            getDriver().get("http://ask-qa.portnov.com/#/registration");
-        }else{
-            System.out.println("This site is not supported: " +url);
-        }
+        getDriver().get("http://ask-qa.portnov.com/#/login");
     }
 
     @And("I wait for {int} sec")
     public void iWaitForSec(int sec) throws InterruptedException {
-        Thread.sleep(1000* sec);
+        Thread.sleep(1000*sec);
     }
 
     @When("I type {string} into email field")
@@ -31,7 +25,7 @@ public class QuizStepDefs {
         getDriver().findElement(By.xpath("//input[@formcontrolname='email']")).sendKeys(email);
     }
 
-    @And("I enter {string} as  password")
+    @And("I enter {string} as password")
     public void iEnterAsPassword(String pass) {
         getDriver().findElement(By.xpath("//input[@formcontrolname='password']")).sendKeys(pass);
     }
@@ -46,45 +40,37 @@ public class QuizStepDefs {
         getDriver().findElement(By.xpath("//h5[contains(text(),'"+menuItem+"')]")).click();
     }
 
+
     @When("I type {string} as quiz title")
     public void iTypeAsQuizTitle(String quizTitle) {
         getDriver().findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys(quizTitle);
     }
 
-    @And("I add a question")
-    public void iAddAQuestion() {
+    @And("I add question")
+    public void iAddQuestion() {
         getDriver().findElement(By.xpath("//mat-icon[contains(text(),'add_circle')]")).click();
     }
 
     @When("I select {string} question type")
     public void iSelectQuestionType(String questionType) {
-        getDriver().findElement(By.xpath("//*[contains(text(),'"+questionType+"')]")).click();
+        getDriver().findElement(By.xpath("//*[contains(text(),'Textual')]")).click();
     }
 
-    @When("I type {string} into {string}")
+    @When("I  type {string} into {string}")
     public void iTypeInto(String questionText, String questionNumber) {
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+questionNumber+"')]/../../..//textarea[@formcontrolname='question']")).sendKeys(questionText);
-    }
-
-    @And("I type {string} as {string} into {string}")
-    public void iTypeAsInto(String optionText, String optionNumber, String questionNumber) {
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+questionNumber+"')]/../../..//textarea[@placeholder='"+optionNumber+"']")).sendKeys(optionText);
-    }
-
-    @When("I select {string} as correct option in {string}")
-    public void iSelectAsCorrectOptionIn(String optionNumber, String questionNumber) {
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+questionNumber+"')]/../../..//textarea[@placeholder='"+optionNumber+"']/../../../../../mat-radio-button")).click();
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q1')]/../../..//textarea[@formcontrolname='question']")).sendKeys(questionText);
     }
 
     @Then("quiz {string} should be displayed on the list of quizzes")
     public void quizShouldBeDisplayedOnTheListOfQuizzes(String quizTitle) {
-        assertThat(getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+quizTitle+"')]")).isDisplayed()).isTrue();
+        assertThat(getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Test Quiz - Igor')]")).isDisplayed()).isTrue();
+
     }
 
     @And("I delete {string}")
     public void iDelete(String quizTitle) throws InterruptedException {
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+quizTitle+"')]")).click();
-        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+quizTitle+"')]/../../..//span[text()='Delete']")).click();
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Test Quiz - Igor')]")).click();
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Test Quiz - Igor')]/../../..//span[text()='Delete']")).click();
         getDriver().findElement(By.xpath("//div[@class='mat-dialog-actions']//span[text()='Delete']")).click();
         Thread.sleep(1000);
     }
