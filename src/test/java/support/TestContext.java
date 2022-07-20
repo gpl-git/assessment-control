d4babd3cb521ebb05c67f8921113551371e1ff4f
“larissaomalley” <larissaomalley@Larissas-MacBook-Pro-2.local>
2022-07-20 07:27:36 +0600

    registrationEmailLarissa

--- src/test/java/support/TestContext.java
+++ src/test/java/support/TestContext.java
@@ -31,7 +31,7 @@ public class TestContext {
     }
 
     public static void initialize() {
-        initialize("firefox", "local", false);
+        initialize("chrome", "local", false);
     }
 
     public static void teardown() {
