6ae363fb85e6161ee29075efa2939a468a7a559f
Maxkalin1 <max800ka@gmail.com>
2022-10-23 22:07:19 -0700

    "version5"

--- src/test/java/predefined/MaksymStepsDefs.java
+++ src/test/java/predefined/MaksymStepsDefs.java
@@ -8,46 +8,100 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class MaksymStepsDefs {
-    @Given("I navigate to {string} web site")
-    public void iNavigateToWebSite(String page) {
-        getDriver().navigate().to(page);
-        }
+  @Given("I navigate to {string} web site")
+  public void iNavigateToWebSite(String page) {
+    getDriver().navigate().to(page);
+  }
 
-    @Then("I type {string} into search field")
-    public void iTypeIntoSearchField(String searchInfo) {
-        getDriver().findElement(By.name("q")).sendKeys(searchInfo);
-    }
+  @Then("I type {string} into search field")
+  public void iTypeIntoSearchField(String searchInfo) {
+    getDriver().findElement(By.name("q")).sendKeys(searchInfo);
+  }
 
-    @Then("I click on find button")
-    public void iClickOnFindButton() {
-        getDriver().findElement(By.id("search_icon")).click();
-    }
+  @Then("I click on find button")
+  public void iClickOnFindButton() {
+    getDriver().findElement(By.id("search_icon")).click();
+  }
 
-    @Then("I wait for {int} seconds")
-    public void iWaitForSeconds(int seconds) throws InterruptedException {
-        Thread.sleep(seconds* 1000L);
-    }
+  @Then("I wait for {int} seconds")
+  public void iWaitForSeconds(int seconds) throws InterruptedException {
+    Thread.sleep(seconds * 1000L);
+  }
 
-    @Then("search result {string} should be present")
-    public void searchResultShouldBePresent(String ExpectedSearchRes) {
-        String actualSearchRes = getDriver().findElement(By.name("q")).getAttribute("value");
-        assertThat(actualSearchRes.equals(ExpectedSearchRes)).isTrue();
+  @Then("search result {string} should be present")
+  public void searchResultShouldBePresent(String ExpectedSearchRes) {
+    String actualSearchRes = getDriver().findElement(By.name("q")).getAttribute("value");
+    assertThat(actualSearchRes.equals(ExpectedSearchRes)).isTrue();
+  }
 
-    }
+  @Then("I navigate to back")
+  public void iNavigateToBack() {
+    getDriver().navigate().back();
+  }
 
-    @Then("I navigate to back")
-    public void iNavigateToBack() {
-        getDriver().navigate().back();
-    }
-
-    @Then("I refresh the current page")
-    public void iRefreshTheCurrentPage() {
+  @Then("I refresh the current page")
+  public void iRefreshTheCurrentPage() {
     getDriver().navigate().refresh();
-    }
+  }
 
+  @Then("I click on Images link")
+  public void iClickOnImagesLink() {
+    getDriver().findElement(By.id("images")).click();
+  }
 
-    @Then("I click on Images link")
-    public void iClickOnImagesLink() {
-        getDriver().findElement(By.id("images")).click();
-    }
+  @Then("logo should be present")
+  public void logoShouldBePresent() {
+    assertThat(
+            getDriver()
+                .findElement(By.xpath("//div[@id='logo']//img[@src='/images/logo.svg']"))
+                .isDisplayed())
+        .isTrue();
+  }
+
+  @Then("I click {string} from ALL CONVERTERS LIST")
+  public void iClickFromALLCONVERTERSLIST(String converterType) {
+    getDriver().findElement(By.xpath("//a[contains(text(),'" + converterType + "')]")).click();
+  }
+
+  @Then("I choose {string} from convertres sub-list")
+  public void iChooseFromConvertresSubList(String converterKind) {
+    getDriver().findElement(By.xpath("//a[contains(text(),'" + converterKind + "')]")).click();
+  }
+
+  @Then("the converter name must match with the header")
+  public void theConverterNameMustMatchWithTheHeader() {}
+
+  @Then("the converter header name contains {string}")
+  public void theConverterNameMustMatchWithTheHeader(String expConvName) {
+    String actualConvName =
+        getDriver().findElement(By.xpath("//h1[contains(text(),'Power Converter')]")).getText();
+    assertThat(actualConvName.contains(expConvName)).isTrue();
+  }
+
+  @Then("I choose {string} from FROM collumn list")
+  public void iChooseFromFROMCollumnList(String optionFrom) {
+    getDriver()
+        .findElement(
+            By.xpath("//select[@id='ucfromunit']/option[text()='"+optionFrom+"']"))
+        .click();
+  }
+
+  @Then("I choose {string} from TO collumn list")
+  public void iChooseFromTOCollumnList(String optionTo) {
+    getDriver()
+        .findElement(
+            By.xpath("//select[@id='uctounit']/option[contains(@value,'"+optionTo+"')]"))
+        .click();
+  }
+
+  @Then("I set {string} to From value")
+  public void iSetToFromValue(String setValue) {
+    getDriver().findElement(By.name("ucfrom")).sendKeys(setValue);
+  }
+
+  @Then("I verify expected calculation result is {string}")
+  public void iVerifyExpectedCalculationResultIs(String expectedRes) {
+    String actualRes = getDriver().findElement(By.id("ucto")).getAttribute("value");
+    assertThat(actualRes.equals(expectedRes)).isTrue();
+  }
 }
--- src/test/resources/features/MaksymK.feature
+++ src/test/resources/features/MaksymK.feature
@@ -15,4 +15,42 @@
       Then I click on Images link
       Then I wait for 3 seconds
 
+      @gitPractice2
+      Scenario: Engeneering Power Conwerter
+        Given I navigate to "https://www.unitconverters.net/" web site
+        Then logo should be present
+        Then I wait for 1 seconds
+        Then I click "Common Converters" from ALL CONVERTERS LIST
+        Then I wait for 1 seconds
+        Then I click "Engineering Converters" from ALL CONVERTERS LIST
+        Then I wait for 1 seconds
+        Then I choose "Power" from convertres sub-list
+        Then I wait for 1 seconds
+        Then the converter header name contains "Power"
+        Then I wait for 1 seconds
+        Then I set "500" to From value
+        Then I wait for 1 seconds
+        Then I choose "watt [W]" from FROM collumn list
+        Then I wait for 2 seconds
+        Then I choose "kilowatt [kW]" from TO collumn list
+        Then I verify expected calculation result is "0.5"
 
+    @gitPractice3
+    Scenario Outline: For Engeneering Power Conwerter ALL POSITIONS
+      Given I navigate to "https://www.unitconverters.net/" web site
+      Then logo should be present
+      Then I click "Common Converters" from ALL CONVERTERS LIST
+      Then I click "Engineering Converters" from ALL CONVERTERS LIST
+      Then I choose <converterKind> from convertres sub-list
+      Then the converter header name contains <expConvName>
+      Then I set <setValue> to From value
+      Then I choose <optionFrom> from FROM collumn list
+      Then I choose <optionTo> from TO collumn list
+      Then I verify expected calculation result is <expectedRes>
+      Examples:
+        | converterKind | expConvName    | setValue      | optionFrom           |   optionTo                    | expectedRes    |
+        | "Power"       | "Power"        | "50"          | "watt [W]"           | "kilowatt [kW]"               | "0.05"         |
+        | "Power"       | "Power"        | "500"         | "watt [mW]"          | "centiwatt [cW]"              | "50000"        |
+        | "Power"       | "Power"        | "140"         | "milliwatt [W]"      | "kilowatt [kW]"               | "0.5"          |
+        | "Power"       | "Power"        | "7850"        | "MBH"                | "kilowatt [kW]"               | "2300.607"     |
+        | "Power"       | "Power"        | "4755"        | "dekawatt [daW]"     | "kilowatt [kW]"               | "47.55"        |
