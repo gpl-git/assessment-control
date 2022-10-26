07b285027ab103a1f6981e2832bec14c4e3ffde5
Maxkalin1 <max800ka@gmail.com>
2022-10-26 02:09:46 -0700

    default feature and Definitions are restored, home work 75% done

--- src/test/java/definitions/QuizStepDefs.java
+++ src/test/java/definitions/QuizStepDefs.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class QuizStepDefs {
-    @Given("I open {string} page [MKs]")
+    @Given("I open {string} page")
     public void iOpenPage(String url) {
         if (url.equals("login")){
             getDriver().get("http://ask-qa.portnov.com/#/login");
@@ -22,128 +22,79 @@ public class QuizStepDefs {
 
     }
 
-    @And("I wait for {int} sec [MKs]")
+    @And("I wait for {int} sec")
     public void iWaitForSec(int sec) throws InterruptedException {
         Thread.sleep(sec*1000);
     }
 
-    @When("I type {string} for teacher user [MKs]")
+    @When("I type {string} for teacher user")
     public void iTypeForTeacherUser(String email) {
         getDriver().findElement(By.xpath("//*[@formcontrolname='email']")).sendKeys(email);
     }
 
-    @And("I type {string} as password [MKs]")
+    @And("I type {string} as password")
     public void iTypeAsPassword(String password) {
         getDriver().findElement(By.xpath("//*[@formcontrolname='password']")).sendKeys(password);
     }
 
-    @When("I click {string} button [MKs]")
+    @When("I click {string} button")
     public void iClickButton(String btnName) {
         getDriver().findElement(By.xpath("//span[contains(text(),'"+btnName+"')]")).click();
     }
 
-    @Then("{string} page will be displayed [MKs]")
+    @Then("{string} page will be displayed")
     public void pageWillBeDisplayed(String expectedUrl) {
         String curUrl = getDriver().getCurrentUrl();
       assertThat(curUrl.contains(expectedUrl)).isTrue();
     }
 
-    @When("I click on {string} menu item [MKs]")
+    @When("I click on {string} menu item")
     public void iClickOnMenuItem(String menuItem) {
         getDriver().findElement(By.xpath("//h5[contains(text(),'"+menuItem+"')]")).click();
     }
 
-    @When("I type {string} as quiz title [MKs]")
+    @When("I type {string} as quiz title")
     public void iTypeAsQuizTitle(String title) {
         getDriver().findElement(By.xpath("//*[@formcontrolname='name']")).sendKeys(title);
     }
 
-    @And("I add a question [MKs]")
+    @And("I add a question")
     public void iAddAQuestion() {
         getDriver().findElement(By.xpath("//mat-icon[text()='add_circle']")).click();
     }
 
-    @When("I select {string} question [MKs]")
+    @When("I select {string} question")
     public void iSelectQuestion(String questionType) {
         getDriver().findElement(By.xpath("//*[contains(text(),'"+questionType+"')]")).click();
     }
 
-    @When("I type {string} into {string} [MKs]")
+    @When("I type {string} into {string}")
     public void iTypeInto(String questionText, String questionNum) {
         getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+questionNum+"')]/../../..//textarea[@formcontrolname='question']")).sendKeys(questionText);
 
     }
 
-    @And("I type {string} as {string} in {string} [MKs]")
+    @And("I type {string} as {string} in {string}")
     public void iTypeAsIn(String optionText, String optionNum, String questionNum) {
         getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+questionNum+"')]/../../..//textarea[@placeholder='"+optionNum+"']")).sendKeys(optionText);
     }
 
 
-    @Then("I select {string} as correct option in {string} [MKs]")
+    @Then("I select {string} as correct option in {string}")
     public void iSelectAsCorrectOptionIn(String optionNum, String questionNum) {
         getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+questionNum+"')]/../../..//textarea[@placeholder='"+ optionNum+ "']/../../../../../mat-radio-button")).click();
     }
 
-    @Then("quiz {string} is displayed on the list of quizzes [MKs]")
+    @Then("quiz {string} is displayed on the list of quizzes")
     public void quizIsDisplayedOnTheListOfQuizzes(String title) {
         assertThat(getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+title+"')]")).isDisplayed()).isTrue();
     }
 
-    @And("I delete quiz {string} [MKs]")
+    @And("I delete quiz {string}")
     public void iDeleteQuiz(String title) throws InterruptedException {
-    getDriver()
-        .findElement(By.xpath("//mat-panel-title[contains(text(),'" + title + "')]"))
-        .click();
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+title+"')]")).click();
         getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+title+"' )]/../../..//*[contains(text(),'Delete')]")).click();
         getDriver().findElement(By.xpath("//div[@class='mat-dialog-actions']//*[contains(text(),'Delete')]")).click();
         Thread.sleep(1000);
-    }
-
-    @Then("I type {string} into FirstOption [MKs]")
-    public void iTypeIntoFirstOption(String text) {
-        getDriver().findElement(By.xpath("//textarea[@placeholder='Option 1*']")).sendKeys(text);
-    }
-
-    @Then("I type {string} into SecondOption [MKs]")
-    public void iTypeIntoSecondOption(String text) {
-        getDriver().findElement(By.xpath("//textarea[@placeholder='Option 2*']")).sendKeys(text);
-    }
-
-    @Then("required quontity of characters should be present in OptionFiedld{int} [MKs]")
-    public void requiredQuontityOfCharactersShouldBePresentInOptionFiedld(int arg0) {
-
-    }
-
-    @Then("required quantity of characters {string} should be present in OptionField1 [MKs]")
-    public void requiredQuantityOfCharactersShouldBePresentInOptionFeild(String expText) {
-        String actText = getDriver().findElement(By.xpath("//textarea[@placeholder='Option 1*']")).getText();
-        assertThat(actText.equals(expText)).isTrue();
-    }
-
-    @Then("required quantity of characters {string} should not be present in OptionField1 [MKs]")
-    public void requiredQuantityOfCharactersShouldNotBePresentInOptionField(String expText) {
-        String actText = getDriver().findElement(By.xpath("//textarea[@placeholder='Option 1*']")).getText();
-        assertThat(actText.equals(expText)).isFalse();
-    }
-
-    @And("I click on Save button [MKs]")
-    public void iClickOnSaveButtonMKs() {
-        getDriver().findElement(By.xpath("//span[contains(text(),'Save')]")).click();
-    }
-
-    @Then("Option1 quiz error should be displayed [MKs]")
-    public void optionQuizErrorShouldBeDisplayed() {
-        assertThat(getDriver().findElement(By.xpath("//mat-error[@id='mat-error-2']")).isDisplayed()).isTrue();
-    }
-
-    @And("I click on element with xpath {string} [MKs]")
-    public void iClickOnElementWithXpathMKs(String path) {
-        getDriver().findElement(By.xpath(path)).click();
-    }
-
-    @Then("error messages should not be displayed [MKs]")
-    public void errorMessagesShouldNotBeDisplayed() {
-        assertThat(getDriver().findElement(By.xpath("//mat-error[@id='mat-error-5']")).isDisplayed()).isFalse();
     }
 }
