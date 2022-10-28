fdae28c1ba5aaaed92e1beb49e16b9cae100d5d2
LiSag2022 <lsagitovar@gmail.com>
2022-10-27 23:07:07 -0400

    Fixed confirmation message

--- src/test/java/definitions/liliyaSagStepdefs.java
+++ src/test/java/definitions/liliyaSagStepdefs.java
@@ -5,6 +5,8 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
@@ -20,13 +22,13 @@ public class liliyaSagStepdefs {
         }
     }
 
-    @When("I click on {string} button")
-    public void iClickOnButton(String RegisterNow) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterNow + "')]")).click();
+    @When("I click on {string} button ls")
+    public void iClickOnButton(String btnName) {
+        getDriver().findElement(By.xpath("//span[contains(text(),'" + btnName + "')]")).click();
     }
 
 
-    @When("I type {string} as a name")
+    @When("I type {string} as a first name")
     public void iTypeAsAName(String firstName) {
         getDriver().findElement(By.xpath("//*[@formcontrolname='firstName']")).sendKeys(firstName);
     }
@@ -59,68 +61,11 @@ public class liliyaSagStepdefs {
 
     }
 
-    @Then("I click on {string} button ls")
-    public void iClickOnButtonLs(String RegisterMe) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterMe + "')]")).click();
-
-    }
-
-    @Then("{string} page should be displayed ls")
+    @Then("{string} message should be displayed ls")
     public void pageShouldBeDisplayedLs(String reg) {
-        if (reg.equals("registration-confirmation")) {
-            getDriver().get("http://ask-qa.portnov.com/#/registration-confirmation");
-        } else {
-            System.out.println("This url is not supported: " + reg);
-        }
-    }
+        String confirmationMess=getDriver().findElement(By.xpath("//h4")).getText();
+        assertThat(confirmationMess.equals(reg)).isTrue();
 
-
-    @Given("I open {string} page as the teacher")
-    public void iOpenPageAsTheTeacher(String url) {
-        if (url.equals("registration")) {
-            getDriver().get("http://ask-qa.portnov.com/#/registration");
-        } else if (url.equals("login")) {
-            getDriver().get("http://ask-qa.portnov.com/#/login");
-        } else {
-            System.out.println("This url is not supported: " + url);
-        }
-    }
-
-    @When("I click at {string} button")
-    public void iClickAtButton(String RegisterNow) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterNow + "')]")).click();
-    }
-
-    @When("I type {string} as the name")
-    public void iTypeAsTheName(String firstName) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='firstName']")).sendKeys(firstName);
-    }
-
-    @And("I type {string} as the last name")
-    public void iTypeAsTheLastName(String lastName) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='lastName']")).sendKeys(lastName);
-    }
-
-    @And("I type {string} as the group code")
-    public void iTypeAsTheGroupCode(String group) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='group']")).sendKeys(group);
-
-    }
-
-    @And("I type {string} as the password")
-    public void iTypeAsThePassword(String password) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='password']")).sendKeys(password);
-
-    }
-
-    @And("I type {string} as the password confirmation")
-    public void iTypeAsThePasswordConfirmation(String confirmPassword) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='confirmPassword']")).sendKeys(confirmPassword);
-    }
-
-    @When("I click on {string} button l")
-    public void iClickOnButtonL(String RegisterMe) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterMe + "')]")).click();
 
     }
 
@@ -129,256 +74,7 @@ public class liliyaSagStepdefs {
         assertThat(getDriver().findElement(By.xpath("//*[contains(text(),'" + field + "')]")).isDisplayed()).isTrue();
 
     }
-
-    @Given("I open {string} page as a teacher ls")
-    public void iOpenPageAsATeacherLs(String url) {
-        if (url.equals("registration")) {
-            getDriver().get("http://ask-qa.portnov.com/#/registration");
-        } else if (url.equals("login")) {
-            getDriver().get("http://ask-qa.portnov.com/#/login");
-        } else {
-            System.out.println("This url is not supported: " + url);
-        }
-    }
-
-    @When("I click at {string} button l")
-    public void iClickAtButtonL(String RegisterNow) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterNow + "')]")).click();
-    }
-
-    @When("I type {string} as the name l")
-    public void iTypeAsTheNameL(String firstName) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='firstName']")).sendKeys(firstName);
-    }
-
-    @And("I type {string} as the last name l")
-    public void iTypeAsTheLastNameL(String lastName) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='lastName']")).sendKeys(lastName);
-    }
-
-    @And("I type {string} as email l")
-    public void iTypeAsEmailL(String email) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='email']")).sendKeys(email);
-    }
-
-    @And("I type {string} as the group code l")
-    public void iTypeAsTheGroupCodeL(String group) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='group']")).sendKeys(group);
-
-    }
-
-    @And("I type {string} as a password l")
-    public void iTypeAsAPasswordL(String password) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='password']")).sendKeys(password);
-
-
-    }
-
-    @And("I type {string} as the password confirmation l")
-    public void iTypeAsThePasswordConfirmationL(String confirmPassword) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='confirmPassword']")).sendKeys(confirmPassword);
-
-
-    }
-
-    @When("I click on the {string} button")
-    public void iClickOnTheButton(String RegisterMe) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterMe + "')]")).click();
-    }
-
-    @Then("{string} error should be displayed l")
-    public void errorShouldBeDisplayedL(String validEmail) {
-        assertThat(getDriver().findElement(By.xpath("//*[contains(text(),'" + validEmail + "')]")).isDisplayed()).isTrue();
-
-    }
-
-    @Given("I open {string} page as a teacher lsl")
-    public void iOpenPageAsATeacherLsl(String url) {
-        if (url.equals("registration")) {
-            getDriver().get("http://ask-qa.portnov.com/#/registration");
-        } else if (url.equals("login")) {
-            getDriver().get("http://ask-qa.portnov.com/#/login");
-        } else {
-            System.out.println("This url is not supported: " + url);
-        }
-    }
-
-    @When("I click at {string} button ls")
-    public void iClickAtButtonLs(String RegisterNow) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterNow + "')]")).click();
-    }
-
-    @When("I type {string} as the name ls")
-    public void iTypeAsTheNameLs(String firstName) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='firstName']")).sendKeys(firstName);
-    }
-
-    @And("I type {string} as the last name ls")
-    public void iTypeAsTheLastNameLs(String lastName) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='lastName']")).sendKeys(lastName);
-    }
-
-    @And("I type {string} as email ls")
-    public void iTypeAsEmailLs(String email) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='email']")).sendKeys(email);
-    }
-
-    @And("I type {string} as the group code ls")
-    public void iTypeAsTheGroupCodeLs(String group) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='group']")).sendKeys(group);
-    }
-
-    @And("I type {string} as a password ls")
-    public void iTypeAsAPasswordLs(String password) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='password']")).sendKeys(password);
-    }
-
-    @And("I type {string} as the password confirmation ls")
-    public void iTypeAsThePasswordConfirmationLs(String confirmPassword) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='confirmPassword']")).sendKeys(confirmPassword);
-
-    }
-
-    @When("I click on the {string} button l")
-    public void iClickOnTheButtonL(String RegisterMe) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterMe + "')]")).click();
-
-    }
-
-    @Then("{string} error should be displayed ls")
-    public void errorShouldBeDisplayedLs(String alert) {
-        assertThat(getDriver().findElement(By.xpath("//*[contains(text(),'" + alert + "')]")).isDisplayed()).isTrue();
-
-        }
-
-    @Given("I open {string} page as a teacher ll")
-    public void iOpenPageAsATeacherLl(String url) {
-        if (url.equals("registration")) {
-            getDriver().get("http://ask-qa.portnov.com/#/registration");
-        } else if (url.equals("login")) {
-            getDriver().get("http://ask-qa.portnov.com/#/login");
-        } else {
-            System.out.println("This url is not supported: " + url);
-        }
-
-    }
-
-    @When("I click at {string} button ll")
-    public void iClickAtButtonLl(String RegisterNow) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterNow + "')]")).click();
-    }
-
-    @When("I type {string} as the name ll")
-    public void iTypeAsTheNameLl(String firstName) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='firstName']")).sendKeys(firstName);
-    }
-
-    @And("I type {string} as the last name ll")
-    public void iTypeAsTheLastNameLl(String lastName) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='lastName']")).sendKeys(lastName);
-            }
-
-    @And("I type {string} as email ll")
-    public void iTypeAsEmailLl(String email) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='email']")).sendKeys(email);
-        
-    }
-
-    @And("I type {string} as the group code ll")
-    public void iTypeAsTheGroupCodeLl(String group) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='group']")).sendKeys(group);
-    }
-
-    @And("I type {string} as a password ll")
-    public void iTypeAsAPasswordLl(String password) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='password']")).sendKeys(password);
-    }
-
-    @And("I type {string} as the password confirmation ll")
-    public void iTypeAsThePasswordConfirmationLl(String confirmPassword) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='confirmPassword']")).sendKeys(confirmPassword);
-        
-    }
-
-    @When("I click on the {string} button ll")
-    public void iClickOnTheButtonLl(String RegisterMe) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterMe + "')]")).click();
-    }
-
-    @Then("{string} error should be displayed ll")
-    public void errorShouldBeDisplayedLl(String alert) {
-       assertThat(getDriver().findElement(By.xpath("//*[contains(text(),'" + alert + "')]")).isDisplayed()).isTrue();
-
-        }
-
-    @Given("I open {string} page as a teacher ss")
-    public void iOpenPageAsATeacherSs(String url) {
-             if (url.equals("registration")) {
-                getDriver().get("http://ask-qa.portnov.com/#/registration");
-            } else if (url.equals("login")) {
-                getDriver().get("http://ask-qa.portnov.com/#/login");
-            } else {
-                System.out.println("This url is not supported: " + url);
-            }
-        }
-
-    @When("I click at {string} button ss")
-    public void iClickAtButtonSs(String RegisterNow) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterNow + "')]")).click();
-
-    }
-
-    @When("I type {string} as the name ss")
-    public void iTypeAsTheNameSs(String firstName) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='firstName']")).sendKeys(firstName);
-    }
-
-    @And("I type {string} as the last name ss")
-    public void iTypeAsTheLastNameSs(String lastName) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='lastName']")).sendKeys(lastName);
-    }
-
-    @And("I type {string} as email ss")
-    public void iTypeAsEmailSs(String email) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='email']")).sendKeys(email);
-    }
-
-    @And("I type {string} as the group code ss")
-    public void iTypeAsTheGroupCodeSs(String group) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='group']")).sendKeys(group);
-    }
-
-
-    @And("I type {string} as a password ss")
-    public void iTypeAsAPasswordSs(String password) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='password']")).sendKeys(password);
-        
-        
-    }
-
-    @And("I type {string} as the password confirmation ss")
-    public void iTypeAsThePasswordConfirmationSs(String confirmPassword) {
-        getDriver().findElement(By.xpath("//*[@formcontrolname='confirmPassword']")).sendKeys(confirmPassword);
-    }
-
-    @When("I click on the {string} button ss")
-    public void iClickOnTheButtonSs(String RegisterMe) {
-        getDriver().findElement(By.xpath("//span[contains(text(),'" + RegisterMe + "')]")).click();
-
-
-    }
-
-    @Then("{string} error should be displayed ss")
-    public void errorShouldBeDisplayedSs(String alert) {
-        assertThat(getDriver().findElement(By.xpath("//*[contains(text(),'" + alert + "')]")).isDisplayed()).isTrue();
-    }
 }
-
-
-
-
-
-
 
 
 
--- src/test/resources/features/lilyaS_Registration.feature
+++ src/test/resources/features/lilyaS_Registration.feature
@@ -6,98 +6,100 @@ Feature: Quiz Scenarios Registration Email
   @quizHW1
   Scenario: Register with valid credentials
     Given I open "login" page as a teacher
-    When I click on "Register Now" button
+    When I click on "Register Now" button ls
     And I wait for 2 sec
-    When I type "Li" as a name
+    When I type "Li" as a first name
     And I type "Sag" as a last name
     And I type "redheadedok1e@mizii.eu" as email
     And I type "Sep" as a group code
     And I type "123456" as a password
     And I type "123456" as a password confirmation
-    And I wait for 2 sec
     When I click on "Register Me" button ls
-    Then "registration-confirmation" page should be displayed ls
+    And I wait for 2 sec
+    Then "You have been Registered." message should be displayed ls
     And I wait for 2 sec
 
   @quizHW2
     Scenario: Register with empty email
-    Given I open "login" page as the teacher
-    When I click at "Register Now" button
+    Given I open "login" page as a teacher
+    When I click on "Register Now" button ls
     And I wait for 2 sec
-    When I type "Ro" as the name
-    And I type "QA" as the last name
-    And I type "Dec" as the group code
-    And I type "1234567" as the password
-    And I type "1234567" as the password confirmation
+    When I type "Li" as a first name
+    And I type "Sag" as a last name
+    And I type "Sep" as a group code
+    And I type "123456" as a password
+    And I type "123456" as a password confirmation
     And I wait for 2 sec
-    When I click on "Register Me" button l
+    When I click on "Register Me" button ls
     Then "This field is required" error should be displayed
     And I wait for 2 sec
 
   @quizHW3
   Scenario: Register with email more than 128 characteristics
-    Given I open "login" page as a teacher ls
-    When I click at "Register Now" button l
+    Given I open "login" page as a teacher
+    When I click on "Register Now" button ls
     And I wait for 2 sec
-    When I type "Rosa" as the name l
-    And I type "Ros" as the last name l
-    And I type "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@mizii.eu" as email l
-    And I type "Dec" as the group code l
-    And I type "654321" as a password l
-    And I type "654321" as the password confirmation l
+    When I type "Li" as a first name
+    And I type "Sag" as a last name
+    And I type "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@mizii.eu" as email
+    And I type "Sep" as a group code
+    And I type "123456" as a password
+    And I type "123456" as a password confirmation
     And I wait for 2 sec
-    When I click on the "Register Me" button
-    Then "Should be a valid email address" error should be displayed l
+    When I click on "Register Me" button ls
+    Then "Should be a valid email address" error should be displayed
     And I wait for 2 sec
 
   @quizHW4
   Scenario: Register with an empty spaces in email filed
-    Given I open "login" page as a teacher lsl
-    When I click at "Register Now" button ls
+    Given I open "login" page as a teacher
+    When I click on "Register Now" button ls
     And I wait for 2 sec
-    When I type "Rosa" as the name ls
-    And I type "Ros" as the last name ls
-    And I type "aaa aaa@mizii.eu" as email ls
-    And I type "Dec" as the group code ls
-    And I type "654321" as a password ls
-    And I type "654321" as the password confirmation ls
+    When I type "Li" as a first name
+    And I type "Sag" as a last name
+    And I type "aaa aaa@mizii.eu" as email
+    And I type "Sep" as a group code
+    And I type "123456" as a password
+    And I type "123456" as a password confirmation
     And I wait for 2 sec
-    When I click on the "Register Me" button l
-    Then "Should be a valid email address" error should be displayed ls
+    When I click on "Register Me" button ls
+    Then "Should be a valid email address" error should be displayed
     And I wait for 2 sec
 
   @quizHW5
   Scenario: Register without top level domain
-    Given I open "login" page as a teacher ll
-    When I click at "Register Now" button ll
+    Given I open "login" page as a teacher
+    When I click on "Register Now" button ls
     And I wait for 2 sec
-    When I type "Rosa" as the name ll
-    And I type "Ros" as the last name ll
-    And I type "aaaaa@gmail" as email ll
-    And I type "Dec" as the group code ll
-    And I type "654321" as a password ll
-    And I type "654321" as the password confirmation ll
+    When I type "Li" as a first name
+    And I type "Sag" as a last name
+    And I type "aaaaa@gmail" as email
+    And I type "Sep" as a group code
+    And I type "654321" as a password
+    And I type "654321" as a password confirmation
     And I wait for 2 sec
-    When I click on the "Register Me" button ll
-    Then "Should be a valid email address" error should be displayed ll
+    When I click on "Register Me" button ls
+    Then "Should be a valid email address" error should be displayed
     # Bug SEP22-281
     And I wait for 2 sec
 
    @quizHW6
    Scenario: Register with email without @ sign
-     Given I open "login" page as a teacher ss
-     When I click at "Register Now" button ss
+     Given I open "login" page as a teacher
+     When I click on "Register Now" button ls
      And I wait for 2 sec
-     When I type "Rosa" as the name ss
-     And I type "Ros" as the last name ss
-     And I type "aaaagmail" as email ss
-     And I type "Dec" as the group code ss
-     And I type "654321" as a password ss
-     And I type "654321" as the password confirmation ss
+     When I type "Rosa" as a first name
+     And I type "Ros" as a last name
+     And I type "aaaagmail" as email
+     And I type "Dec" as a group code
+     And I type "654321" as a password
+     And I type "654321" as a password confirmation
      And I wait for 2 sec
-     When I click on the "Register Me" button ss
-     Then "Should be a valid email address" error should be displayed ss
+     When I click on "Register Me" button ls
+     Then "Should be a valid email address" error should be displayed
      And I wait for 2 sec
+
+
 
 
 
