3989d58040ecd0148b520627fc226f5da71feb0b
Maxkalin1 <max800ka@gmail.com>
2022-10-29 21:11:59 -0700

    some useless parts of code deleted [jira SEP22-653]

--- src/test/java/definitions/MaksymSingleChoiseOptionsStepDefs.java
+++ src/test/java/definitions/MaksymSingleChoiseOptionsStepDefs.java
@@ -143,14 +143,11 @@ public class MaksymSingleChoiseOptionsStepDefs {
   }
 
   @And("I click on Save button ")
-  public void iClickOnSaveButtonMKs() {
+  public void iClickOnSaveButton() {
     getDriver().findElement(By.xpath("//span[contains(text(),'Save')]")).click();
   }
 
-  @And("I click on element with xpath {string} ")
-  public void iClickOnElementWithXpathMKs(String path) {
-    getDriver().findElement(By.xpath(path)).click();
-  }
+
 
   @Then("error messages should not be displayed ")
   public void errorMessagesShouldNotBeDisplayed() {
