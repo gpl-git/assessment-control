768944605b90870ae4221f69f45c3c270956e1d6
Deep <deepnashu@yahoo.com>
2023-04-04 16:04:00 -0700

    UpdatedRegistrationFName

--- src/test/java/definitions/FnameyStepDefs.java
+++ src/test/java/definitions/FnameyStepDefs.java
@@ -4,6 +4,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 
 
@@ -12,49 +13,24 @@ import java.util.stream.IntStream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
+
+
 public class FnameyStepDefs {
     @Given("I navigate  to {string} page")
     public void iNavigateToPage(String url) {
-        if(url.equals("login")) {
+        if (url.equals("login")) {
             getDriver().get("http://ask-qa.portnov.com/#/login");
-        }else if (url.equals("registration")) {
+        } else if (url.equals("registration")) {
             getDriver().get("http://ask-qa.portnov.com/#/registration");
-        }else{
-                    System.out.println(url + " is not supported");
-                }
-            }
-
-
-
-
-    @And("I click register me button")
-    public void iClickRegisterMeButton() {
-        getDriver().findElement(By.xpath("//span[contains(text(),'Register Me')]")).click();
+        } else {
+            System.out.println(url + " is not supported");
+        }
     }
 
-
-    @Then("fname error message should be displayed")
-    public void fnameErrorMessageShouldBeDisplayed() {
-        assertThat(getDriver().findElement(By.xpath("//mat-error[@id='mat-error-0']")).isDisplayed()).isTrue();
-    }
-
-    @And("fname error message {string} should be displayed")
-    public void fnameErrorMessageShouldBeDisplayed(String expmessage) {
-        String actMessage = getDriver().findElement(By.xpath("//mat-error[@id='mat-error-0']")).getText();
-        assertThat(actMessage.equals(expmessage)).isTrue();
-    }
 
     @When("I type {string} into fname field")
     public void iTypeIntoFnameField(String fname) {
         getDriver().findElement(By.xpath("//input[@formcontrolname='firstName']")).sendKeys(fname);
-
-    }
-
-
-    @Then("fname  white space error message  {string}should be displayed")
-    public void fnameWhiteSpaceErrorMessageShouldBeDisplayed(String expMessage) {
-        String actMessage =getDriver().findElement(By.xpath("//mat-error[@id='mat-error-0']")).getText();
-        assertThat(actMessage.equals(expMessage)).isTrue();
 
     }
 
@@ -63,31 +39,82 @@ public class FnameyStepDefs {
         getDriver().findElement(By.xpath("//input[@formcontrolname='firstName']")).sendKeys(fname);
     }
 
-    @Then("fname error message should not be displayed")
-    public void fnameErrorMessageShouldNotBeDisplayed() {
-        assertThat(getDriver().findElement(By.xpath("//mat-error[@id='mat-error-0']")).isDisplayed()).isFalse();
+
+    @And("I type {string} into last Name field")
+    public void iTypeIntoLastNameField(String lName) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='lastName']")).sendKeys(lName);
     }
 
-    @And("I clear fname field")
-    public void iClearFnameField() {
-        getDriver().findElement(By.xpath("//input[@formcontrolname='firstName']")).clear();
+    @And("I type {string}into email field")
+    public void iTypeIntoEmailField(String email) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='email']")).sendKeys(email);
+    }
+
+    @And("I type {string} into group code field")
+    public void iTypeIntoGroupCodeField(String gCode) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='group']")).sendKeys(gCode);
+    }
+
+    @And("I type{string} into password field")
+    public void iTypeIntoPasswordField(String password) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='password']")).sendKeys(password);
+    }
+
+    @And("I type{string}into Confirm password field")
+    public void iTypeIntoConfirmPasswordField(String cPassword) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='confirmPassword']")).sendKeys(cPassword);
     }
 
 
-    @When("I type maxium character {string}into fname field")
-    public void iTypeMaxiumCharacterIntoFnameField(String fname) {
-        getDriver().findElement(By.xpath("//input[@formcontrolname='firstName']")).sendKeys(fname);
-
+    @Then("{string} Confirmation message should be display")
+    public void confirmationMessageShouldBeDisplay(String message) {
+        String messageText = getDriver().findElement(By.xpath("//h4")).getText();
+        assertThat(message.equals(messageText)).isTrue();
     }
 
-    @When("I type more than maxium cha {string} into fname field")
-    public void iTypeMoreThanMaxiumChaIntoFnameField(String fname) {
-        getDriver().findElement(By.xpath("//input[@formcontrolname='firstName']")).sendKeys(fname);
+    @When("I click {string}button")
+    public void iClickButton(String registerMe) {
+        getDriver().findElement(By.xpath("//span[text()='Register Me']")).click();
     }
 
-    @Then("NO more than maxium char error message{string} should be diplayed")
-    public void noMoreThanMaxiumCharErrorMessageShouldBeDiplayed(String expMessage) {
-        String actMessage =getDriver().findElement(By.xpath("//mat-error[@id='mat-error-0']")).getText();
+
+    @Then("{string} error message should be displayed")
+    public void errorMessageShouldBeDisplayed(String expMessage) {
+        String actMessage = getDriver().findElement(By.xpath("//mat-error")).getText();
         assertThat(actMessage.equals(expMessage)).isTrue();
     }
+
+    @Then("{string} error message shouls be dispalyed")
+    public void errorMessageShoulsBeDispalyed(String expMessage) {
+        String actMessage = getDriver().findElement(By.xpath("//mat-error")).getText();
+        assertThat(actMessage.equals(expMessage)).isTrue();
+    }
+
+    @When("I type {int} alphanumaric charater into fname field")
+    public void iTypeAlphanumaricCharaterIntoFnameField(int num) {
+        boolean useLetters = true;
+        boolean useNumbers = true;
+        String generatedString = RandomStringUtils.random(num, useLetters, useNumbers);
+//        System.out.println(generatedString);
+        getDriver().findElement(By.xpath("//input[@formcontrolname='firstName']")).sendKeys(generatedString);
+    }
+
+    @When("I type {int} character into fname field")
+    public void iTypeCharacterIntoFnameField(int num) {
+        boolean useLetters = true;
+        boolean useNumbers = true;
+        String generatedString = RandomStringUtils.random(num, useLetters, useNumbers);
+//        System.out.println(generatedString);
+        getDriver().findElement(By.xpath("//input[@formcontrolname='firstName']")).sendKeys(generatedString);
+
+    }
+
+
+    @Then("Error message one{string}should be displayed")
+    public void errorMessageOneShouldBeDisplayed(String expMessage) {
+        String actMessage = getDriver().findElement(By.xpath("//mat-error")).getText();
+        assertThat(actMessage.equals(expMessage)).isTrue();
+
+    }
 }
+
--- src/test/resources/features/AshuRegistrationFname.feature
+++ src/test/resources/features/AshuRegistrationFname.feature
@@ -2,30 +2,66 @@
 
   Feature: Registration First Name
 
+#    Background:
+#      Given I navigate  to "registration" page
+#      And I type "ABC" into last Name field
+#      And I type "123@gmail.com"into email field
+#      And I type "AAA" into group code field
+#      And I type"12345" into password field
+#      And I type"12345"into Confirm password field
+
+
 
     @Fname
     Scenario: Verify Registration First Name field
       Given I navigate  to "registration" page
-      And I click register me button
-      Then fname error message should be displayed
-      And fname error message "This field is required" should be displayed
-      And I wait for 2 sec
-      When I type "    AA  " into fname field
-      Then fname  white space error message  "Whitespaces are not allowed"should be displayed
-      And I wait for 2 sec
-      And I clear fname field
-      When I type "A3!sdghfn" into the fname field
-#      Then fname error message should not be displayed
-      And I wait for 2 sec
-      And I clear fname field
-      When I type maxium character "gcjvcvcvcgdgcjdkhc86868eg3eegttdghgdsed67e78dwsdgsajgdc67ed8tdjhgdjsgdjsgsddtsudtusjgdajsdjasgd876d8w6diwydgasjdgdsjdd87686diolydsgjdgjsad2876e23oieegwedje876dywiuedgwjdgqwe7682eugwdjgdjkhe87623euygdegdjgqwe6271euwdgwjdgejkwfue8712eyiywgdjgdjefuyggwewgeg"into fname field
-      And I wait for 2 sec
-      And I clear fname field
-      When I type more than maxium cha "gcjvcvcvcgdgcjdkhc86868eg3eegttdghgdsed67e78dwsdgsajgdc67ed8tdjhgdjsgdjsgsddtsudtusjgdajsdjasgd876d8w6diwydgasjdgdsjdd87686diolydsgjdgjsad2876e23oieegwedje876dywiuedgwjdgqwe7682eugwdjgdjkhe87623euygdegdjgqwe6271euwdgwjdgejkwfue8712eyiywgdjgdjefuyggwewgegddsd" into fname field
-      Then NO more than maxium char error message"No more than 254 charaters is not allowed " should be diplayed
-      And I wait for 2 sec
+      When I type "Ashu" into the fname field
+      And I type "ABC" into last Name field
+      And I type "123@gmail.com"into email field
+      And I type "AAA" into group code field
+      And I type"12345" into password field
+      And I type"12345"into Confirm password field
+#      When I type "Ashu" into the fname field
+      When I click "register me"button
+      Then "You have been Registered." Confirmation message should be display
+      And I wait for 3 sec
 
+    @firstName
+    Scenario Outline: Verify Registration First Name field -outline
+      When I type <fname> into the fname field
+      When I click <registerMe>button
+      Examples:
+        | fname  | registerMe    |
+        | "A!" | "register me" |
+        | "A!@@" | "register me" |
+        | "12345" | "register me" |
+        | "!asd_#" | "register me" |
+        | "@#$%^" | "register me" |
 
+      @fnameError
+      Scenario: Verify "Fields is required" error message
+        And I click "register me"button
+        Then "This field is required" error message should be displayed
+        And I wait for 3 sec
+
+      @fnameWhitespacesErrorMessage
+      Scenario: Verify "White Spaces are not allowed " error message
+        When I type "    Ashu    " into fname field
+        And I click "register me"button
+        Then "Whitespaces are not allowed" error message shouls be dispalyed
+        And I wait for 3 sec
+
+       @fNameMaxiumChar
+       Scenario: Verify Maximum Character are accepted
+         When I type 254 alphanumaric charater into fname field
+         And I wait for 3 sec
+         And I click "register me"button
+
+         @fnameMoreThanMaximumChar
+         Scenario: Verify 255 characters are not allowed
+           When I type 255 character into fname field
+           Then Error message one"No more than 254 character"should be displayed
+           # It's a known bug .Application accepts 254 character and doesn'tthrow an error message"More than 254 character is not allowed"
 
 
 
