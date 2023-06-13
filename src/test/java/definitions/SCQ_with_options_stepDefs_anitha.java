9e55c0b2291f729c60e19dcfca17ba821f894b2b
Anithas26 <anithanagarathinam@gmail.com>
2023-06-13 09:32:13 -0700

    single choice question feature file and step defs are created

---
+++ src/test/java/definitions/SCQ_with_options_stepDefs_anitha.java
@@ -0,0 +1,166 @@
+package definitions;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static support.TestContext.getDriver;
+
+public class SCQ_with_options_stepDefs_anitha {
+
+    @Given("I navigate to {string} page")
+    public void iNavigateToPage(String page) {
+        switch (page) {
+            case "login":
+                getDriver().get("http://ask-qa.portnov.com/#/login");
+                break;
+            case "register":
+                getDriver().get("http://ask-qa.portnov.com/#/registration");
+                break;
+            case "forgot_password":
+                getDriver().get("http://ask-qa.portnov.com/#/forgot-password");
+                break;
+        }
+    }
+
+    @When("I enter the teachers email {string} into email field")
+    public void iEnterTheTeachersEmailIntoEmailField(String email) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='email']")).sendKeys(email);
+    }
+
+    @And("I enter the password {string} into password field")
+    public void iEnterThePasswordIntoPasswordField(String password) {
+        getDriver().findElement(By.xpath("//input[@formcontrolname='password']")).sendKeys(password);
+    }
+
+    @And("I click the {string} button")
+    public void iClickTheButton(String btnName) {
+        getDriver().findElement(By.xpath("//span[contains(text(),'" + btnName + "')]")).click();
+    }
+
+    @And("I wait for {int} sec for the presence of all the elements on the page")
+    public void iWaitForSecForThePresenceOfAllTheElementsOnThePage(long time) throws InterruptedException {
+        getDriver().manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
+    }
+
+
+    @Then("I should see the text {string} present in the home page")
+    public void iShouldSeeTheTextPresentInTheHomePage(String txtPresence) {
+        getDriver().findElement(By.xpath("//p[contains(text(),'" + txtPresence + "')]")).isDisplayed();
+    }
+
+
+    @And("I click the {string} menu item from the home page")
+    public void iClickTheMenuItemFromTheHomePage(String menuItem) {
+        getDriver().findElement(By.xpath("//h5[contains(text(),'" + menuItem + "')]")).click();
+    }
+
+    @And("I click {string} button under the list of quizzes section")
+    public void iClickButtonUnderTheListOfQuizzesSection(String btnName) {
+        getDriver().findElement(By.xpath("//span[contains(text(),'" + btnName + "')]")).click();
+    }
+
+    @When("I type {string} in the quiz title field")
+    public void iTypeInTheQuizTitleField(String quizTitle) {
+        getDriver().findElement(By.xpath("//*[@formcontrolname='name']")).sendKeys(quizTitle);
+    }
+
+    @And("I add a question by clicking the button Add question")
+    public void iAddAQuestionByClickingTheButtonAddQuestion() {
+        getDriver().findElement(By.xpath("//mat-icon[text()='add_circle']")).click();
+    }
+
+    @And("I select the question type as {string}")
+    public void iSelectTheQuestionTypeAs(String questionType) {
+        getDriver().findElement(By.xpath("//*[contains(text(),'" + questionType + "')]")).click();
+    }
+
+    @And("I type the question as {string} into question field of {string}")
+    public void iTypeTheQuestionAsIntoQuestionFieldOf(String questText, String questNum) {
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + questNum + "')]/following::textarea[@formcontrolname='question']")).sendKeys(questText);
+    }
+
+    @And("I type {string} into the {string} of {string}")
+    public void iTypeIntoTheOf(String optText, String optNum, String questNum) {
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + questNum + "')]/following::textarea[@placeholder='" + optNum + "*']")).sendKeys(optText);
+    }
+
+
+    @And("I select the {string} from the list of options for question {string}")
+    public void iSelectTheFromTheListOfOptionsForQuestion(String optionNumber, String questNum) {
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + questNum + "')]/following::textarea[@placeholder='" + optionNumber + "*']/preceding::mat-radio-button[1]")).click();
+    }
+
+    @Then("I should see the quiz name {string} in the list of quizzes section")
+    public void iShouldSeeTheQuizNameInTheListOfQuizzesSection(String quizTitle) {
+        List<WebElement> titles = getDriver().findElements(By.xpath("mat-panel-title"));
+        for (WebElement title : titles) {
+            if (title.getText().contains(quizTitle)) {
+                title.isDisplayed();
+                title.click();
+            }
+        }
+    }
+
+    @And("I delete the quiz name {string} from the list of quizzes")
+    public void iDeleteTheQuizNameFromTheListOfQuizzes(String quizTitle) {
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + quizTitle + "')]")).click();
+        getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'" + quizTitle + "')]/following::span[text()='Delete']")).click();
+        getDriver().findElement(By.xpath("//ac-modal-confirmation//span[text()='Delete']")).click();
+    }
+
+
+    @Then("I should see an error message on the snack-bar")
+    public void iShouldSeeAnErrorMessageOnTheSnackBar() {
+        getDriver().findElement(By.xpath("//simple-snack-bar[contains(text(),'Quiz is not completed. Check highlighted')]")).isDisplayed();
+    }
+
+    @And("I should an error message on the {string} field")
+    public void iShouldAnErrorMessageOnTheField(String option) {
+        getDriver().findElement(By.xpath("//textarea[@placeholder='" + option + "*']/following::mat-error[contains(text(),'This field is required')]")).isDisplayed();
+    }
+
+    @And("I should see an error message {string}")
+    public void iShouldSeeAnErrorMessage(String errorMsg) {
+        getDriver().findElement(By.xpath("//*[contains(text(),'" + errorMsg + "')]")).isDisplayed();
+    }
+
+    @And("I click the {string} from the list of quizzes section")
+    public void iClickTheFromTheListOfQuizzesSection(String quizTitle) throws InterruptedException {
+
+//        WebElement title= getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+quizTitle+"')]"));
+//        WebDriverWait wait = new WebDriverWait(getDriver(), 10);
+//        wait.until(ExpectedConditions.elementToBeClickable(title));
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//mat-panel-title[contains(text(),'"+quizTitle+"')]")));
+//        Actions action = new Actions(getDriver());
+//        action.moveToElement(title);
+
+//        WebElement ele = getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'\"+quizTitle+\"')]"));
+//        JavascriptExecutor executor = (JavascriptExecutor)getDriver();
+//        executor.executeScript("arguments[0].click();", ele);
+
+        WebElement element = getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+quizTitle+"')]"));
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+        Thread.sleep(500);
+        element.click();
+    }
+
+    @And("I click the {string} button until")
+    public void iClickTheButtonUntil(String btnName) throws InterruptedException {
+        WebElement btn_Name = getDriver().findElement(By.xpath("//span[contains(text(),'" +btnName+"')]"));
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", btn_Name);
+        Thread.sleep(500);
+        btn_Name.click();
+
+    }
+}
---
+++ src/test/resources/features/scq_with_options_anitha.feature
@@ -0,0 +1,92 @@
+@single_choice_question_options
+Feature:Single Choice Question - Options
+
+  Background:
+    Given I navigate to "login" page
+    When I enter the teachers email "suteneko@rapidbeos.net" into email field
+    And I enter the password "test123" into password field
+    And I click the "Sign In" button
+    And I wait for 2 sec for the presence of all the elements on the page
+    Then I should see the text "TEACHER" present in the home page
+    When I click the "Quizzes" menu item from the home page
+    And I click "Create New Quiz" button under the list of quizzes section
+
+  @positive #Issue ID:MAY23-359
+  Scenario:Option 1 and Option 2 are filled out, the radio button is checked
+    When I type "Single Choice Question with two options" in the quiz title field
+    And I add a question by clicking the button Add question
+    And I select the question type as "Single-Choice"
+    And I type the question as "What is BDD?" into question field of "Q1"
+    And I type "Behavior Driven Development" into the "Option 1" of "Q1"
+    And I type "Behavior Driven Deployment" into the "Option 2" of "Q1"
+    And I select the "Option 1" from the list of options for question "Q1"
+    And I click the "Save" button
+    Then I should see the quiz name "Single Choice Question with two options" in the list of quizzes section
+    And I delete the quiz name "Single Choice Question with two options" from the list of quizzes
+
+  @negative #Issue ID: MAY23-341
+  Scenario:Options 1 and 2 are empty, the radio button is not checked
+    When I type "Single Choice Question with empty options" in the quiz title field
+    And I add a question by clicking the button Add question
+    And I select the question type as "Single-Choice"
+    And I type the question as "What is BDD?" into question field of "Q1"
+    And I click the "Save" button
+    Then I should see an error message on the snack-bar
+    And I should an error message on the "Option 1" field
+    And I should an error message on the "Option 2" field
+
+  @negative #Issue ID: MAY23-349
+  Scenario:Options 1 and 2 are empty, the radio button is checked
+    When I type "Single Choice Question with options empty and radio button checked" in the quiz title field
+    And I add a question by clicking the button Add question
+    And I select the question type as "Single-Choice"
+    And I type the question as "What is BDD?" into question field of "Q1"
+    And I select the "Option 1" from the list of options for question "Q1"
+    And I click the "Save" button
+    Then I should see an error message on the snack-bar
+    And I should an error message on the "Option 1" field
+    And I should an error message on the "Option 2" field
+
+  @negative #Issue ID: MAY23-354
+  Scenario:Option 1 is filled out, Options 2 is empty, the radio button is checked
+    When I type "Single Choice Question with option1 filled and option2 empty" in the quiz title field
+    And I add a question by clicking the button Add question
+    And I select the question type as "Single-Choice"
+    And I type the question as "What is BDD?" into question field of "Q1"
+    And I type "Behavior Driven Development" into the "Option 1" of "Q1"
+    And I select the "Option 1" from the list of options for question "Q1"
+    And I click the "Save" button
+    Then I should see an error message on the snack-bar
+    And I should an error message on the "Option 2" field
+
+  @negative #Issue ID: MAY23-357
+  Scenario: Option 1 and Option 2 are filled out, the radio button is not checked
+    When I type "Single Choice Question with radio button unchecked" in the quiz title field
+    And I add a question by clicking the button Add question
+    And I select the question type as "Single-Choice"
+    And I type the question as "What is BDD?" into question field of "Q1"
+    And I type "Behavior Driven Development" into the "Option 1" of "Q1"
+    And I type "Behavior Driven Deployment" into the "Option 2" of "Q1"
+    And I click the "Save" button
+    Then I should see an error message on the snack-bar
+    And I should see an error message "*Choose correct answer"
+
+  @positive #Issue ID: MAY23-365
+  Scenario:Allowable characters on option field with Alphanumeric and Sp. characters
+    When I type "Single Choice allowable characters" in the quiz title field
+    And I add a question by clicking the button Add question
+    And I select the question type as "Single-Choice"
+    And I type the question as "What is BDD?" into question field of "Q1"
+    And I type "sT8uKp2R9`~@#$%^&*()_+|={}[]:”;’<>?,./®©£¥¢¦§«»€" into the "Option 1" of "Q1"
+    And I type "sT8uKp2R9`~@#$%^&*()_+|={}[]:”;’<>?,./®©£¥¢¦§«»€" into the "Option 2" of "Q1"
+    And I select the "Option 1" from the list of options for question "Q1"
+    And I click the "Save" button
+    And I wait for 2 sec for the presence of all the elements on the page
+    Then I should see the quiz name "Single Choice allowable characters" in the list of quizzes section
+#    And I click the "Single Choice allowable characters" from the list of quizzes section
+    And I wait for 2 sec for the presence of all the elements on the page
+    And I click the "Preview" button until
+
+
+
+
