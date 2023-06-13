efe1b7129fbe195ea3b3a801f4f1e1f5544cfe08
srkrishnapp <srkrishnapp.com>
2023-06-12 23:29:28 -0700

    Edited the script with scenario that allows more than one showstopper

--- src/test/java/definitions/Krish_textStepDefs.java
+++ src/test/java/definitions/Krish_textStepDefs.java
@@ -4,12 +4,12 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static support.TestContext.getDriver;
 
 public class Krish_textStepDefs {
@@ -56,13 +56,9 @@ public class Krish_textStepDefs {
     }
 
     @And("I add question")
-    public void iAddQuestion() {
+    public void iAddQuestion() throws InterruptedException {
         getDriver().findElement(By.xpath("//mat-icon[text()='add_circle']")).click();
-    }
-
-    @When("I choose {string} question type")
-    public void iChooseQuestionType(String qType) {
-        getDriver().findElement(By.xpath("//*[contains(text(),'"+qType+"')]")).click();
+        Thread.sleep(1000);
     }
 
     @And("I enter {string} into field of {string}")
@@ -70,10 +66,10 @@ public class Krish_textStepDefs {
         getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'"+Qno+"')]/../../..//textarea[@formcontrolname='question']")).sendKeys(Qn);
     }
 
-    @Then("I click {string} checkbox")
+   /* @Then("I click {string} checkbox")
     public void iClickCheckbox(String txt) {
         getDriver().findElement(By.xpath("//span[contains(text(),'"+txt+"')]")).click();
-    }
+    }*/
 
     @Then("{string} is displayed near the question")
     public void isDisplayedNearTheQuestion(String txt) {
@@ -92,5 +88,39 @@ public class Krish_textStepDefs {
             }
 
         }
+    }
+
+
+    @Then("check * is displayed near both question as show stopper")
+    public void checkIsDisplayedNearBothQuestionAsShowStopper() {
+        WebElement Q1 = getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q1')]"));
+        WebElement Q2 = getDriver().findElement(By.xpath("//mat-panel-title[contains(text(),'Q2')]"));
+        if(Q1.getText().contains("*")){
+            System.out.println("Q1 is the showstopper");
+        }else if (Q2.getText().contains("*")){
+            System.out.println("Q2 is the showstopper");
+        }else if(Q1.getText().contains("*") && Q2.getText().contains("*")){
+            System.out.println("Its a bug");
+        }
+        else{
+            System.out.println("no showstopper");
+        }
+
+
+    }
+
+    @When("I choose {string} question type for qn number {int}")
+    public void iChooseQuestionTypeForQnNumber(String qType, int no) {
+        getDriver().findElement(By.xpath("(//div[@class='mat-radio-label-content' and contains(text(),'"+qType+"')])["+no+"]")).click();
+    }
+
+    @Then("new panel for qn {int} is displayed")
+    public void newPanelForQnIsDisplayed(int no) {
+        assertThat(getDriver().findElement(By.xpath("(//div[@class='mat-radio-label-content' and contains(text(), 'Single-Choice')])["+no+"]")).isDisplayed()).isTrue();
+    }
+
+    @Then("I click {string} checkbox for qn {int}")
+    public void iClickCheckboxForQn(String txt, int no) {
+        getDriver().findElement(By.xpath("(//span[contains(text(),'"+txt+"')])["+no+"]")).click();
     }
 }
--- src/test/resources/features/krish_textual_showstopper.feature
+++ src/test/resources/features/krish_textual_showstopper.feature
@@ -14,11 +14,11 @@
     Scenario: Create a quiz with textual showstopper
       When I give "Krishna_textual_showstopper" as quiz title
       And I add question
-      When I choose "Textual" question type
+      When I choose "Textual" question type for qn number 1
       Then I waited for 1 sec
       And I enter "Describe Risk Analysis?" into field of "Q1"
       And I waited for 1 sec
-      Then I click "Show-Stopper" checkbox
+      Then I click "Show-Stopper" checkbox for qn 1
       And I waited for 1 sec
       And I press "Preview" button
       Then "Show-Stopper Question" is displayed near the question
@@ -28,3 +28,24 @@
       And I press "Save" button
       And I waited for 1 sec
       Then "Krishna_textual_showstopper" is displayed on the list
+      And I waited for 1 sec
+      @texts2
+      Scenario: Verify only one show stopper per quiz
+        When I give "textual_showstopper_krish" as quiz title
+        And I add question
+        When I choose "Textual" question type for qn number 1
+        Then I waited for 1 sec
+        And I enter "What is Qa" into field of "Q1"
+        And I waited for 1 sec
+        Then I click "Show-Stopper" checkbox for qn 1
+        And I waited for 1 sec
+        And I add question
+        Then new panel for qn 2 is displayed
+        When I choose "Textual" question type for qn number 2
+        Then I waited for 1 sec
+        And I enter "What is Scrum" into field of "Q2"
+        And I waited for 1 sec
+        Then I click "Show-Stopper" checkbox for qn 2
+        And I waited for 1 sec
+        Then check * is displayed near both question as show stopper
+        And I waited for 1 sec
