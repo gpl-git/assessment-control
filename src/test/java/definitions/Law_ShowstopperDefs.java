1ddcd33c50ee3b8b4a735bc5c72abba09a294ca1
LawRentz <law.dominguez27@gmail.com>
2023-06-15 13:12:38 -0700

    Law.feature update

--- src/test/java/definitions/Law_ShowstopperDefs.java
+++ src/test/java/definitions/Law_ShowstopperDefs.java
@@ -5,6 +5,10 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import support.TestContext;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
@@ -20,17 +24,17 @@ public class Law_ShowstopperDefs {
     }
 
 
-    @When("I type {string} into e-input")
+    @When("I type {string} into e-input Law")
     public void iTypeIntoEInput(String email) {
         getDriver().findElement(By.xpath("//*[@formcontrolname='email']")).sendKeys(email);
     }
 
-    @Then("I type {string} into p-input")
+    @Then("I type {string} into p-input Law")
     public void iTypeIntoPInput(String password) {
         getDriver().findElement(By.xpath("//*[@formcontrolname='password']")).sendKeys(password);
     }
 
-    @Then("I click {string} btn")
+    @Then("I click {string} btn Law")
     public void iClickBtn(String btnName) {
         getDriver().findElement(By.xpath("//span[contains(text(),'Sign In')]")).click();
     }
@@ -45,7 +49,7 @@ public class Law_ShowstopperDefs {
         getDriver().findElement(By.xpath("//*[@formcontrolname='name']")).sendKeys(quizTitle);
     }
 
-    @Then("I add the first question")
+    @Then("I add the first question Law")
     public void iAddTheFirstQuestion() {
         getDriver().findElement(By.xpath("//mat-icon[text()='add_circle']")).click();
     }
@@ -92,10 +96,109 @@ public class Law_ShowstopperDefs {
     @Then("I preview {string} to verify Showstopper is displayed")
     public void iPreviewToVerifyShowstopperIsDisplayed(String quizTitle) {
         getDriver().findElement(By.xpath(
-                "//mat-panel-title[contains(text(),"+quizTitle+"')]")).click();
+                "//mat-panel-title[contains(text(),'"+quizTitle+"')]")).click();
         getDriver().findElement(By.xpath(
                 "//mat-panel-title[contains(text(),'"+quizTitle+"')]/../../..//span[text()='Preview']")).click();
 
 
     }
+
+
+
+
+    @And("I verify {string} displays {string}")
+    public void iVerifyDisplays(String xpath, String text) {
+        String actualText = getDriver().findElement(By.xpath(xpath)).getText();
+        assertThat(actualText).isEqualTo(text);
+    }
+
+    @Then("I click {string} to edit the quiz")
+    public void iClickToEditTheQuiz(String quizTitle) {
+        getDriver().findElement(By.xpath(
+                "//mat-panel-title[contains(text(),'Law Showstopper')]")).click();
+        getDriver().findElement(By.xpath(
+                "//mat-panel-title[contains(text(),'Law Showstopper')]/../../..//span[text()='Edit']")).click();
+    }
+
+
+
+
+    @Then("{string} displays {string} as a showstopper")
+    public void displaysAsAShowstopper(String questionNum, String asterisk) {
+        String result = getDriver().findElement(By.xpath("//*[contains(text(),'"+questionNum+"')]")).getText();
+        assertThat(result.contains(asterisk)).isTrue();
+    }
+
+
+    @When("I check {string} in {string} Law")
+    public void iCheckInLaw(String checkbox, String questionNum) {
+        getDriver().findElement(By.xpath("//mat-checkbox[@id='mat-checkbox-2']")).click();
+    }
+
+
+    @Then("I click on {string} as correct answer Law")
+    public void iClickOnAsCorrectAnswerLaw(String checkbox) {
+        getDriver().findElement(By.xpath("//mat-radio-button[@id='mat-radio-6']")).click();
+    }
+
+
+    @Then("I verify {string} is showing Law")
+    public void iVerifyIsShowingLaw(String text) throws InterruptedException {
+        String res = getDriver().findElement(By.xpath("//p[contains(@class,'show-stopper')]")).getText();
+        System.out.println(res);
+        Thread.sleep(1000);
+        assertThat(res.contains(text)).isTrue();
+
+    }
+
+    @Then("I add {int} new question Law")
+    public void iAddNewQuestion(int num) {
+            getDriver().findElement(By.xpath("//mat-icon[contains(text(),'add_circle')]")).click();
+
+        }
+
+    @Then("I create a new question")
+    public void iCreateANewQuestion(String questionType) {
+        getDriver().findElement(By.xpath("//input[@id='mat-radio-14-input']")).click();
+    }
+
+    @And("I choose {string} the question type Law")
+    public void iChooseTheQuestionTypeLaw(String questionType) {
+        getDriver().findElement(By.xpath("")).click();
+    }
+
+    @Then("I add the second question Law")
+    public void iAddTheSecondQuestionLaw() {
+        getDriver().findElement(By.xpath("//mat-icon[text()='add_circle']")).click();
+    }
+
+    @Then("I click {string} in {string} Law")
+    public void iClickInLaw(String checkBox, String questionNum) {
+        getDriver().findElement(By.xpath("//mat-checkbox[@id='mat-checkbox-17-input']")).click();
+    }
+
+    @When("I select second question type Law")
+    public void iSelectSecondQuestionTypeLaw() {
+        getDriver().findElement(By.xpath("//mat-radio-button[@id='mat-radio-72']")).click();
+    }
+
+    @Then("I close the dialog box Law")
+    public void iCloseTheDialogBoxLaw() {
+        getDriver().findElement(By.xpath("//button[@aria-label='Close dialog']")).click();
+    }
+
+    @And("I delete {string} from the list of quizzes Law")
+    public void iDeleteFromTheListOfQuizzesLaw(String quizTitle) throws InterruptedException {
+        getDriver().findElement(By.xpath(
+                "//mat-panel-title[contains(text(),'"+quizTitle+"')]/../../..//span[text()='Delete']")).click();
+        getDriver().findElement(By.xpath("//ac-modal-confirmation//span[text()='Delete']")).click();
+        Thread.sleep(1000);
+    }
+
+
+    @Then("I click {string} button as answer Law")
+    public void iClickButtonAsAnswerLaw(String checkBox) {
+        getDriver().findElement(By.xpath("//mat-radio-button[@id='mat-radio-37']")).click();
+    }
 }
+
--- src/test/resources/features/Law_Showstopper.feature
+++ src/test/resources/features/Law_Showstopper.feature
@@ -1,43 +1,101 @@
 @LawShowStopper
 Feature: Automate Single Choice Question - Showstopper
-  @lawshowstopper1
-  Scenario: Create a quiz with single choice showstopper question
-    #Log In
+  Background:
     Given I go to "ASK Login"
-    When I type "britters@colevillecapital.com" into e-input
-    Then I type "abc123" into p-input
-    Then I click "Sign In" btn
+    When I type "britters@colevillecapital.com" into e-input Law
+    Then I type "abc123" into p-input Law
+    Then I click "Sign In" btn Law
     And I wait for 3 sec
     And I click "Quizzes" menu item
     And I wait for 2 sec
+
+  @lawshowstopper1
+  Scenario: Test Set 1 Verify Question is a Showstopper (Create a quiz)
     When I click "Create New Quiz" button
     And I wait for 2 sec
-    #Create a quiz
     When I type "Law Showstopper" as title for Quiz
-    Then I add the first question
+    Then I add the first question Law
     When I select "Single" question type
     And I wait for 2 sec
     And I type "q1" into qfield of "Q1"
     When I type "a1" into "Option 1" option field of "Q1"
     And I type "a2" into "Option 2" option field of "Q1"
     And I wait for 2 sec
-    Then I click on element with xpath "//mat-radio-button[@id='mat-radio-6']"
+    Then I click on "a1" as correct answer Law
     And I wait for 2 sec
-    #TestSet1 (Showstopper option is displayed)
-    Then element with xpath "//span[contains(text(),'Show-Stopper')]" should contain text "Show-Stopper"
-    Then I click on element with xpath "//mat-checkbox[@id='mat-checkbox-2']"
+    When I check "Show-stopper" in "Q1" Law
     And I wait for 2 sec
-    #TestSet2 (Showstopper * should be required)
-    Then element with xpath "//span[@class='mat-content']" should contain text "Q1*: q1"
+    #TestSet1 (Showstopper * should be required)
+    Then "Q1" displays "*" as a showstopper
     And I wait for 2 sec
     And I click "Save" button
     And I wait for 2 sec
-    #TestSet3 (Verify Showstopper is shown in Preview)
     Then "Law Showstopper" is displayed on the list of quizzes
-#    Then I click on element with xpath "//mat-panel-title[contains(text(),'Law Showstopper')]"
-#    And I wait for 5 sec
-#    Then I click on element with xpath "//mat-panel-title[contains(text(),'Law Showstopper')]/../../..//span[text()='Preview']"
-#    Then element with xpath "//p[contains(text(),'Show-Stopper Question')]" should contain text "Show-Stopper Question"
-#    Then I click on element with xpath "//button[@aria-label='Close dialog']"
-#    And I wait for 5 sec
     And I delete "Law Showstopper" from the list of quizzes
+
+  @lawshowstopper2
+  Scenario: Test Set 2 Verify only 1 Showstopper Question per Quiz
+    When I click "Create New Quiz" button
+    And I wait for 2 sec
+    When I type "Law Showstopper" as title for Quiz
+    Then I add the first question Law
+    When I select "Single" question type
+    And I wait for 2 sec
+    And I type "q1" into qfield of "Q1"
+    When I type "a1" into "Option 1" option field of "Q1"
+    And I type "a2" into "Option 2" option field of "Q1"
+    And I wait for 2 sec
+    Then I click on "a1" as correct answer Law
+    And I wait for 2 sec
+    When I check "Show-stopper" in "Q1" Law
+    And I wait for 2 sec
+    Then "Q1" displays "*" as a showstopper
+    And I wait for 2 sec
+    Then I add the second question Law
+    When I select "Single" question type
+    And I wait for 12 sec
+    And I type "q2" into question field of "Q2"
+    When I type "a1" into "Option 1" option field of "Q2"
+    And I type "a2" into "Option 2" option field of "Q2"
+    And I wait for 4 sec
+#    Then I click "a1" button as answer Law
+#    And I wait for 2 sec
+    Then I click "Show-stopper" in "Q2" Law
+    And I wait for 2 sec
+    Then "Q2" displays "*" as a showstopper
+    And I wait for 2 sec
+    And I click "Save" button
+    And I wait for 2 sec
+    Then "Law Showstopper" is displayed on the list of quizzes
+    And I delete "Law Showstopper" from the list of quizzes
+
+  @lawshowstopper3
+  Scenario: Test Set 3 Verify Showstopper is shown in Preview
+    When I click "Create New Quiz" button
+    And I wait for 2 sec
+    When I type "Law Showstopper" as title for Quiz
+    Then I add the first question Law
+    When I select "Single" question type
+    And I wait for 2 sec
+    And I type "q1" into qfield of "Q1"
+    When I type "a1" into "Option 1" option field of "Q1"
+    And I type "a2" into "Option 2" option field of "Q1"
+    And I wait for 2 sec
+    Then I click on "a1" as correct answer Law
+    And I wait for 2 sec
+    When I check "Show-stopper" in "Q1" Law
+    And I wait for 2 sec
+    Then "Q1" displays "*" as a showstopper
+    And I wait for 2 sec
+    And I click "Save" button
+    And I wait for 2 sec
+    Then "Law Showstopper" is displayed on the list of quizzes
+    And I wait for 3 sec
+    Then I preview "Law Showstopper" to verify Showstopper is displayed
+    And I wait for 3 sec
+    Then I verify "Show-Stopper Question" is showing Law
+    And I wait for 2 sec
+    Then I close the dialog box Law
+    And I wait for 2 sec
+    And I delete "Law Showstopper" from the list of quizzes Law
+
